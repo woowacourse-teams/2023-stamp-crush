61721b13d145e39d03892c5d1f24e32420316c71
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-07-11 15:40:41 +0900

    [FE] feat: 전역으로 사용할 색상 정의

---
+++ frontend/src/style/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  main: '#EBE5CE',
+  main600: '#CABE96',
+  point: '#253C76',
+  gray: '#888888',
+  gray100: '#F9F9F9',
+  gray200: '#F3F3F3',
+  gray300: '#D8D8D8',
+  gray400: '#B7B7B7',
+  black: '#2A1D1F',
+  white: '#FFFFFF',
+};
+
+export const theme: DefaultTheme = {
+  colors,
+};
