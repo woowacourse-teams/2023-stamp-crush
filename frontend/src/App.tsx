7e1bf6848030799615da1adc2cd087be21b6f02a
2yunseong <dbsdltjd3701@naver.com>
2023-07-11 15:38:26 +0900

    feat: 전역으로 사용할 색상 정의

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from 'styled-components';
 import Router from './Router';
+import { theme } from './style/theme';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
