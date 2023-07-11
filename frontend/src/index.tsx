528f01471bc8f9cc275e9b6cb2dbd2fe68aeb8c0
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-11 15:51:27 +0900

    feat: 버튼 색상을 위한 GlobalStyle에 색상 팔레트 정의

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './style/GlobalStyle';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
