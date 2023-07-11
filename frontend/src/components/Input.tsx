d1fd50f09ff580db8bc532ea17ad082f9a8becc6
regularPark <srk99279@naver.com>
2023-07-11 10:13:22 +0900

    feat: `Input` 컴포넌트 생성

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,80 @@
+import { ChangeEventHandler, HTMLInputTypeAttribute } from 'react';
+import { forwardRef } from 'react';
+import styled from 'styled-components';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type?: HTMLInputTypeAttribute;
+  width?: number;
+  placeholder?: string;
+  variant?: 'basic' | 'underlined';
+  maxLength?: number;
+}
+
+type StyledInputProps = {
+  $width?: number;
+  $center?: boolean;
+  disabled?: boolean;
+};
+
+const BaseInput = styled.input<StyledInputProps>`
+  width: ${(props) => (props.$width ? `${props.$width}px` : '100%')};
+  padding: 12px;
+
+  border: none;
+  border-radius: 8px;
+  text-align: ${(props) => (props.$center ? 'center' : 'initial')};
+  font-size: 18px;
+
+  &::placeholder {
+    color: #aaa;
+  }
+
+  &:focus {
+    background: #ddd;
+  }
+`;
+
+const BasicInput = styled(BaseInput)`
+  border: 1px solid #000;
+`;
+
+const UnderlinedInput = styled(BaseInput)`
+  background: transparent;
+  border-radius: 8px;
+  border-bottom: 2px solid #bbb;
+
+  &:focus {
+    border-bottom: 2px solid #ebe5ce;
+    outline: none;
+  }
+`;
+
+const InputVariation = {
+  basic: BasicInput,
+  underlined: UnderlinedInput,
+};
+
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { type, placeholder, width, value, variant, maxLength } = props;
+
+  const InputComponent = InputVariation[variant ?? 'basic'];
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.target;
+    if (maxLength !== undefined && value.length > maxLength) return;
+  };
+
+  return (
+    <InputComponent
+      ref={ref}
+      type={type}
+      $width={width}
+      value={value}
+      placeholder={placeholder}
+      maxLength={maxLength}
+      onChange={handleInputChange}
+    />
+  );
+});
+
+Input.displayName = 'Input';
