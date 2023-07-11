9ee5777396e873b73bd31e6a54ebd2b1846318ad
regularPark <srk99279@naver.com>
2023-07-11 22:23:02 +0900

    feat: `SideBar` 컴포넌트 생성

---
+++ frontend/src/components/SideBar/SideBar.style.tsx
@@ -0,0 +1,72 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+interface SideBarSelect {
+  $isSelected: boolean;
+}
+
+interface SideBarSize {
+  $width: number;
+  $height: number;
+}
+
+type SideBarStyleProps = SideBarSelect & SideBarSize;
+
+export const SideBarContainer = styled.div<SideBarSize>`
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+  width: ${(props) => `${props.$width}px`};
+  height: ${(props) => `${props.$height}px`};
+
+  border-top: 1px solid ${({ theme }) => theme.colors.gray};
+
+  & > :nth-child(n) {
+    border: solid ${({ theme }) => theme.colors.gray};
+    border-width: 0 1px 1px 1px;
+  }
+`;
+
+export const LabelContent = styled.span<SideBarStyleProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  width: ${(props) => `${props.$width}px`};
+  height: ${(props) => `${props.$height}px`};
+
+  font-weight: ${(props) => (props.$isSelected ? 'bold' : 'normal')};
+  color: ${({ theme }) => theme.colors.gray};
+
+  transition: all 0.4s ease;
+  cursor: pointer;
+`;
+
+export const SideBarLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const SideBarContent = styled.div<SideBarSelect>`
+  transition: all 0.4s ease;
+  background-color: ${(props) => `${props.$isSelected ? '#eee' : 'transparent'}`};
+`;
+
+export const Arrow = styled.span`
+  position: relative;
+
+  &::after {
+    position: absolute;
+    left: 0;
+    top: -8px;
+
+    width: 10px;
+    height: 10px;
+
+    border-top: 3px solid ${({ theme }) => theme.colors.gray};
+    border-right: 3px solid ${({ theme }) => theme.colors.gray};
+
+    content: '';
+    transform: rotate(45deg);
+  }
+`;
---
+++ frontend/src/components/SideBar/index.tsx
@@ -0,0 +1,48 @@
+import { useLocation } from 'react-router-dom';
+import {
+  Arrow,
+  LabelContent,
+  SideBarContainer,
+  SideBarContent,
+  SideBarLink,
+} from './SideBar.style';
+
+interface SideBarOptions {
+  key: string;
+  value: string;
+}
+
+interface SideBarProps {
+  width: number;
+  height: number;
+
+  options: SideBarOptions[];
+}
+
+const SideBar = ({ width, height, options }: SideBarProps) => {
+  const current = useLocation().pathname;
+
+  return (
+    <SideBarContainer $width={width} $height={height}>
+      {options.map(({ key, value }) => {
+        console.log(value === current);
+        return (
+          <SideBarContent key={key} $isSelected={value === current}>
+            <SideBarLink to={value}>
+              <LabelContent
+                $isSelected={value === current}
+                $width={width}
+                $height={height / options.length}
+              >
+                {key}
+                <Arrow />
+              </LabelContent>
+            </SideBarLink>
+          </SideBarContent>
+        );
+      })}
+    </SideBarContainer>
+  );
+};
+
+export default SideBar;
