d94307dae703c5f4f3f3f582a1aeaf7362d97cf0
2yunseong <dbsdltjd3701@naver.com>
2023-07-11 18:33:36 +0900

    refactor: 컨벤션에 맞게 스타일링 변경

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+import { HeaderContainer, LogoImg, LogoutButton } from './Header.style';
 import AdminHeaderLogo from '../assets/admin_header_logo.png';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -10,33 +10,13 @@ const Header = () => {
     navigate('/login');
   };
   return (
-    <GlobalHeader>
+    <HeaderContainer>
       <Link to="/">
         <LogoImg src={AdminHeaderLogo} />
       </Link>
       <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
-    </GlobalHeader>
+    </HeaderContainer>
   );
 };
 
 export default Header;
-
-const GlobalHeader = styled.header`
-  display: flex;
-  height: 59px;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 60px;
-  border-bottom: 1px solid #888;
-`;
-
-const LogoImg = styled.img`
-  height: 38px;
-`;
-
-const LogoutButton = styled.button`
-  border: none;
-  background: transparent;
-  color: black;
-  cursor: pointer;
-`;
