c123b5910957a48d5835f9ecf8d2be559f6c1cb6
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-12 11:20:16 +0900

    refactor: StyledButtonProps 타입 추가

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,12 +1,12 @@
 import { ButtonHTMLAttributes } from 'react';
 import { BaseButton } from './Button.style';
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
   size?: 'medium' | 'large';
 }
 
-const Button = ({ variant = 'primary', size = 'medium', ...props }: Props) => {
+const Button = ({ variant = 'primary', size = 'medium', ...props }: ButtonProps) => {
   return <BaseButton {...props} $variant={variant} $size={size} />;
 };
 
