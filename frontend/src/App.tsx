1e0a04b6496144e2cc9a09cacc97078d10be8b59
강영민 <kym6050@hanyang.ac.kr>
2023-07-12 18:11:46 +0900

    [FE] feat: Template component 구현 (#53)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import GlobalStyle from 'style/GlobalStyle';
+import GlobalStyle from './style/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import { theme } from './style/theme';
