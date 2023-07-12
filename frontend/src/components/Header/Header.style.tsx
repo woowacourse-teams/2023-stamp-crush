6604b0bf92cd381389ba31024127c9d2466254da
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-07-12 15:42:28 +0900

    [FE] feat: 사장님 페이지 헤더 컴포넌트 구현 (#32)
    * chore: 사장님 페이지 헤더 로고 추가
    
    * feat: 사장님 페이지 헤더 컴포넌트 스타일링 및 구현
    
    * fix: delete redundant file
    
    * refactor: 컨벤션에 맞게 style 파일 컴포넌트로 병합
    
    * fix: 로그아웃 버튼 요구사항에 맞게 변경
    
    * refactor: 컨벤션에 맞게 스타일링 변경
    
    * fix: 잘못된 경로 수정

---
+++ frontend/src/components/Header/Header.style.tsx
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  height: 59px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 60px;
+  border-bottom: 1px solid #888;
+`;
+
+export const LogoImg = styled.img`
+  height: 38px;
+`;
+
+export const LogoutButton = styled.button`
+  border: none;
+  background: transparent;
+  color: black;
+  cursor: pointer;
+`;
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { HeaderContainer, LogoImg, LogoutButton } from './Header.style';
+import AdminHeaderLogo from '../../assets/admin_header_logo.png';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // log out 로직
+    navigate('/login');
+  };
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <LogoImg src={AdminHeaderLogo} />
+      </Link>
+      <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
