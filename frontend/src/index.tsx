ad6a06f495830fe6a50a5564beb903fcbe52438a
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-12 11:14:58 +0900

    refactor: GlobalStyle App.tsx로 위치 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,10 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyle from './style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
