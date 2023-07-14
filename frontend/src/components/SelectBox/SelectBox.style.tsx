c3c1a50611058ada79b08d9f4bc4d3dcb1272e18
박정규 <srk99279@naver.com>
2023-07-14 16:30:12 +0900

    [FE] feat: `SelectBox` 컴포넌트 구현 (#54)
    * feat: `SelectBox` 컴포넌트 생성
    
    * design: 디자인 수정
    
    * refactor: css 컨벤션 적용
    
    * refactor: 불필요한 코드 제거
    
    * refactor: `expanded`를 `props`로 전달
    
    * style: 개행 추가

---
+++ frontend/src/components/SelectBox/SelectBox.style.tsx
@@ -0,0 +1,109 @@
+import styled, { css } from 'styled-components';
+
+interface StyledSelectBoxProps {
+  $minWidth: number;
+  $minHeight: number;
+  $expanded: boolean;
+}
+
+export const BaseSelectBox = styled.span<StyledSelectBoxProps>`
+  position: relative;
+  display: inline-block;
+  margin-right: 1px;
+  min-height: ${(props) => `${props.$minHeight}px`};
+  max-height: ${(props) => `${props.$minHeight}px`};
+  min-width: ${(props) => `${props.$minWidth}px`};
+  overflow: hidden;
+
+  cursor: pointer;
+  text-align: left;
+  white-space: nowrap;
+
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.colors.main600};
+  border-radius: 4px;
+  background-color: white;
+
+  transition: 0.4s all ease-in-out;
+
+  input:checked + label {
+    display: block;
+    border-top: none;
+    position: absolute;
+    top: 0;
+    width: 100%;
+
+    &:nth-child(2) {
+      margin-top: 0;
+      position: relative;
+    }
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    right: 12px;
+    top: 10px;
+
+    width: 6px;
+    height: 6px;
+
+    border-top: 2px solid ${({ theme }) => theme.colors.gray};
+    border-right: 2px solid ${({ theme }) => theme.colors.gray};
+    transform: rotate(-225deg);
+
+    transition: 0.4s all ease-in-out;
+  }
+
+  ${(props) =>
+    props.$expanded &&
+    css`
+      border: 1px solid ${({ theme }) => theme.colors.point};
+      background: #fff;
+      border-radius: 12px;
+      padding: 0;
+      box-shadow: rgba(0, 0, 0, 0.1) 3px 3px 5px 0px;
+      max-height: 400px;
+
+      label {
+        border-top: 1px solid ${({ theme }) => theme.colors.gray};
+        &:hover {
+          color: ${({ theme }) => theme.colors.main600};
+        }
+      }
+      input:checked + label {
+        color: ${({ theme }) => theme.colors.main600};
+      }
+
+      &::after {
+        transform: rotate(-45deg);
+        top: 12px;
+      }
+    `}
+`;
+
+export const SelectContent = styled.input`
+  width: 1px;
+  height: 1px;
+  display: inline-block;
+  position: absolute;
+  opacity: 0.01;
+`;
+
+export const LabelContent = styled.label`
+  border-top: 1px solid ${({ theme }) => theme.colors.gray};
+  display: block;
+  height: 33px;
+  line-height: 33px;
+  padding-left: 12px;
+  padding-right: 66px;
+  cursor: pointer;
+  position: relative;
+  transition: 0.4s color ease-in-out;
+
+  &:nth-child(2) {
+    margin-top: 33px;
+
+    border-top: 1px solid ${({ theme }) => theme.colors.gray};
+  }
+`;
---
+++ frontend/src/components/SelectBox/index.tsx
@@ -0,0 +1,55 @@
+import { MouseEventHandler, useState } from 'react';
+import { BaseSelectBox, LabelContent, SelectContent } from './SelectBox.style';
+
+export type SelectBoxOption = {
+  key: string;
+  value: string;
+};
+
+interface SelectBoxProps {
+  options: SelectBoxOption[];
+}
+
+const SelectBox = ({ options }: SelectBoxProps) => {
+  const [checkedOption, setCheckedOption] = useState(options[0].value);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpandSelectBox: MouseEventHandler<HTMLInputElement> = (event) => {
+    event.preventDefault();
+
+    setIsExpanded(!isExpanded);
+
+    if (event.target instanceof HTMLLabelElement) {
+      event.target.textContent && setCheckedOption(event.target.textContent);
+    }
+
+    if (event.target instanceof HTMLInputElement) {
+      setCheckedOption(event.target.value);
+    }
+  };
+
+  return (
+    <BaseSelectBox
+      $expanded={isExpanded}
+      $minWidth={120}
+      $minHeight={32}
+      onClick={toggleExpandSelectBox}
+    >
+      {options.map(({ key, value }) => (
+        <>
+          <SelectContent
+            key={key}
+            type="radio"
+            name="sortType"
+            value={value}
+            checked={checkedOption === value}
+            id={key}
+          />
+          <LabelContent htmlFor={key}>{value}</LabelContent>
+        </>
+      ))}
+    </BaseSelectBox>
+  );
+};
+
+export default SelectBox;
