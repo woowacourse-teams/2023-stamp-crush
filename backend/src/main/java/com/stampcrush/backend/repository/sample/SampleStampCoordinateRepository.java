7a4cb338238bfb10baff91c174f8d9de7140199f
gitchan <eunkeeeee@gmail.com>
2023-07-15 14:03:46 +0900

    feat: Sample 조회 관련 Repository 생성

---
+++ backend/src/main/java/com/stampcrush/backend/repository/sample/SampleStampCoordinateRepository.java
@@ -0,0 +1,7 @@
+package com.stampcrush.backend.repository.sample;
+
+import com.stampcrush.backend.entity.sample.SampleStampCoordinate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleStampCoordinateRepository extends JpaRepository<SampleStampCoordinate, Long> {
+}
