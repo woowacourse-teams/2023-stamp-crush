f92664b1bb470a60c35d3d119f74d60053604e47
JunDonghyuk <ehdgur4814@naver.com>
2023-07-16 13:11:41 +0900

    feat: 리워드 목록 조회 api 기능 구현

--- backend/src/main/java/com/stampcrush/backend/api/reward/RewardApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/reward/RewardApiController.java
@@ -1,20 +1,37 @@
 package com.stampcrush.backend.api.reward;
 
+import com.stampcrush.backend.api.reward.dto.request.RewardFindRequest;
 import com.stampcrush.backend.api.reward.dto.request.RewardUsedUpdateRequest;
+import com.stampcrush.backend.api.reward.dto.response.RewardFindResponse;
+import com.stampcrush.backend.api.reward.dto.response.RewardsFindResponse;
 import com.stampcrush.backend.application.reward.RewardService;
+import com.stampcrush.backend.application.reward.dto.RewardFind;
+import com.stampcrush.backend.application.reward.dto.RewardFindResult;
 import com.stampcrush.backend.application.reward.dto.RewardUsedUpdate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class RewardApiController {
 
     private final RewardService rewardService;
+
+    @GetMapping("/customers/{customerId}/rewards")
+    public ResponseEntity<RewardsFindResponse> findRewards(@PathVariable("customerId") Long customerId,
+                                                           @ModelAttribute RewardFindRequest rewardFindRequest) {
+        System.out.println("here");
+        RewardFind rewardFind = new RewardFind(customerId, rewardFindRequest.getCafeId(), rewardFindRequest.isUsed());
+        List<RewardFindResult> rewardFindResults = rewardService.findRewards(rewardFind);
+        List<RewardFindResponse> rewardFindResponses = rewardFindResults.stream()
+                .map(RewardFindResponse::new)
+                .toList();
+        RewardsFindResponse response = new RewardsFindResponse(rewardFindResponses);
+        return ResponseEntity.ok(response);
+    }
 
     @PatchMapping("/customers/{customerId}/rewards/{rewardId}")
     public ResponseEntity<Void> updateRewardUsed(@PathVariable("customerId") Long customerId,
