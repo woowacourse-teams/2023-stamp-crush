51298cbe3a456d3de43dbb84ce98ba5eb058f763
Youngho <cyh6099@gmail.com>
2023-07-16 12:17:09 +0900

    feat: Coupon의 Stamp조회를 위한 양방향 연관관계 설정

--- backend/src/main/java/com/stampcrush/backend/entity/coupon/Coupon.java
+++ backend/src/main/java/com/stampcrush/backend/entity/coupon/Coupon.java
@@ -1,13 +1,16 @@
 package com.stampcrush.backend.entity.coupon;
 
-import com.stampcrush.backend.entity.user.Customer;
 import com.stampcrush.backend.entity.baseentity.BaseDate;
 import com.stampcrush.backend.entity.cafe.Cafe;
+import com.stampcrush.backend.entity.user.Customer;
 import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import static jakarta.persistence.FetchType.EAGER;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -22,9 +25,9 @@ public class Coupon extends BaseDate {
     private LocalDate expiredDate;
 
     @Enumerated(EnumType.STRING)
-    private CouponStatus status;
+    private CouponStatus status = CouponStatus.USING;
 
-    private Boolean deleted;
+    private Boolean deleted = Boolean.TRUE;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "customer_id")
@@ -37,4 +40,17 @@ public class Coupon extends BaseDate {
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "coupon_design_id")
     private CouponDesign couponDesign;
+
+    @OneToMany(mappedBy = "coupon", cascade = CascadeType.ALL, fetch = EAGER)
+    private List<Stamp> stamps = new ArrayList<>();
+
+    public Coupon(LocalDate expiredDate, Customer customer, Cafe cafe, CouponDesign couponDesign) {
+        this.expiredDate = expiredDate;
+        this.customer = customer;
+        this.cafe = cafe;
+        this.couponDesign = couponDesign;
+    }
+
+    protected Coupon() {
+    }
 }
