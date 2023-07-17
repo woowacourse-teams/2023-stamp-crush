64911b0f1065a1f097e59e37add89c84fdb8b04f
yenawee <yaena0319@naver.com>
2023-07-17 16:55:04 +0900

    [BE] feat: 전화번호로 고객을 조회한다/ 임시 고객을 생성한다 (#90)
    * feat: 전화번호로 고객을 조회한다
    
    * feat: 임시 고객을 생성한다
    
    * feat: 전화번호로 고객을 조회한다 API 반환값이 명세와 동일하게 나오게 수정
    
    * refactor : 코드 리뷰 반영 후 리팩토링
    
    - 전화번호로 고객 조회 API 관련 코드 주석 삭제
    - @RequiredArgsConstructor 추가
    - 임시 고객 생성 시 이미 존재하고 있는 전화번호면 예외 처리
    - CustomerService try - catch문 필요 없어 삭제
    - 테스트 코드 assertSoftly 로 변경
    
    ---------
    
    Co-authored-by: youngh0 <cyh6099@gmail.com>

--- backend/src/main/java/com/stampcrush/backend/entity/user/RegisterCustomer.java
+++ backend/src/main/java/com/stampcrush/backend/entity/user/RegisterCustomer.java
@@ -3,7 +3,11 @@ package com.stampcrush.backend.entity.user;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import static lombok.AccessLevel.PROTECTED;
+
+@NoArgsConstructor(access = PROTECTED)
 @Getter
 @DiscriminatorValue("register")
 @Entity
--- backend/src/main/java/com/stampcrush/backend/entity/user/TemporaryCustomer.java
+++ backend/src/main/java/com/stampcrush/backend/entity/user/TemporaryCustomer.java
@@ -2,7 +2,11 @@ package com.stampcrush.backend.entity.user;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
 
+import static lombok.AccessLevel.PROTECTED;
+
+@NoArgsConstructor(access = PROTECTED)
 @DiscriminatorValue("temporary")
 @Entity
 public class TemporaryCustomer extends Customer {
