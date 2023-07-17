2f987a059851271a9bfc54f430bb67441f0b351f
gitchan <eunkeeeee@gmail.com>
2023-07-17 22:12:15 +0900

    fix: SampleStampCoordinate 저장 로직 순서 변경

--- backend/src/test/java/com/stampcrush/backend/acceptance/SampleCouponIntegrationTest.java
+++ backend/src/test/java/com/stampcrush/backend/acceptance/SampleCouponIntegrationTest.java
@@ -2,6 +2,7 @@ package com.stampcrush.backend.acceptance;
 
 import com.stampcrush.backend.entity.sample.SampleBackImage;
 import com.stampcrush.backend.entity.sample.SampleFrontImage;
+import com.stampcrush.backend.entity.sample.SampleStampCoordinate;
 import com.stampcrush.backend.entity.sample.SampleStampImage;
 import com.stampcrush.backend.repository.sample.SampleBackImageRepository;
 import com.stampcrush.backend.repository.sample.SampleFrontImageRepository;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
+import java.util.List;
 
 import static com.stampcrush.backend.fixture.SampleCouponFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -40,22 +43,23 @@ public class SampleCouponIntegrationTest extends IntegrationTest {
         sampleStampCoordinateRepository.deleteAll();
         sampleBackImageRepository.deleteAll();
         sampleStampImageRepository.deleteAll();
-
-        saveSampleCoupon();
     }
 
     private void saveSampleCoupon() {
         SampleFrontImage savedSampleFrontImage = sampleFrontImageRepository.save(SAMPLE_FRONT_IMAGE);
         SampleBackImage savedSampleBackImage = sampleBackImageRepository.save(SAMPLE_BACK_IMAGE);
-        SAMPLE_COORDINATES_SIZE_EIGHT.stream()
+        List<SampleStampCoordinate> savedSampleStampCoordinates = SAMPLE_COORDINATES_SIZE_EIGHT.stream()
+                .peek(e -> e.setSampleBackImage(savedSampleBackImage))
                 .map(e -> sampleStampCoordinateRepository.save(e))
-                .forEach(e -> e.setSampleBackImage(savedSampleBackImage));
+                .toList();
         SampleStampImage savedSampleStampImage = sampleStampImageRepository.save(SAMPLE_STAMP_IMAGE);
     }
 
     @Test
     void 전체_쿠폰_샘플을_조회한다() {
         // given, when
+        saveSampleCoupon();
+
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
@@ -77,6 +81,8 @@ public class SampleCouponIntegrationTest extends IntegrationTest {
     @Test
     void 최대_스탬프_개수로_쿠폰_샘플을_필터링해서_조회한다() {
         // given, when
+        saveSampleCoupon();
+
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
@@ -98,6 +104,8 @@ public class SampleCouponIntegrationTest extends IntegrationTest {
     @Test
     void 해당하는_최대_스탬프_개수의_샘플_쿠폰이_없으면_뒷면이_조회되지_않는다() {
         // given, when
+        saveSampleCoupon();
+
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
