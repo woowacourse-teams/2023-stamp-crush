6c0c4334629413a7131917832ab15d476156ffb0
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-17 15:28:29 +0900

    chore: cypress 설치 및 script 설정

--- frontend/src/pages/Admin/CustomerList/index.tsx
+++ frontend/src/pages/Admin/CustomerList/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const CustomerList = () => {
+  const fetchTest = async () => {
+    const data = await fetch('/todos').then((response) => {
+      console.log(response.json());
+    });
+  };
+  fetchTest();
   return <div>CustomerList</div>;
 };
 
