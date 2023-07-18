b0baab86ea7cc861c283c8d57275bc02032d0f1a
JunDonghyuk <ehdgur4814@naver.com>
2023-07-18 17:58:35 +0900

    feat: entity생성자 및 copy()작성

--- backend/src/main/java/com/stampcrush/backend/entity/cafe/CafePolicy.java
+++ backend/src/main/java/com/stampcrush/backend/entity/cafe/CafePolicy.java
@@ -1,6 +1,7 @@
 package com.stampcrush.backend.entity.cafe;
 
 import com.stampcrush.backend.entity.baseentity.BaseDate;
+import com.stampcrush.backend.entity.coupon.CouponPolicy;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.SQLDelete;
@@ -48,5 +49,9 @@ public class CafePolicy extends BaseDate {
 
     public void delete() {
         this.deleted = true;
+    }
+
+    public CouponPolicy copy() {
+        return new CouponPolicy(maxStampCount, reward, expirePeriod);
     }
 }
