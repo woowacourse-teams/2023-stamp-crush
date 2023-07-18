6eca8ba0d539b8605379b122b55dc9c1dafa68ba
JunDonghyuk <ehdgur4814@naver.com>
2023-07-18 17:59:22 +0900

    refactor: 오탈자 수정

--- backend/src/main/java/com/stampcrush/backend/repository/user/CustomerRepository.java
+++ backend/src/main/java/com/stampcrush/backend/repository/user/CustomerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    
-    List<Customer> findByphoneNumber(String phoneNumber);
+
+    List<Customer> findByPhoneNumber(String phoneNumber);
 }
