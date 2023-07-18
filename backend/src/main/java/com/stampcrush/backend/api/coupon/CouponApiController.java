7c2c8c5d6f0076d74724d8051dd4245243d3df40
JunDonghyuk <ehdgur4814@naver.com>
2023-07-18 18:00:30 +0900

    feat: coupon신규 발급, 스탬프 적립 컨트롤러 구현

--- backend/src/main/java/com/stampcrush/backend/api/coupon/CouponApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/coupon/CouponApiController.java
@@ -1,14 +1,16 @@
 package com.stampcrush.backend.api.coupon;
 
+import com.stampcrush.backend.api.coupon.request.CouponCreateRequest;
+import com.stampcrush.backend.api.coupon.request.StampCreateRequest;
+import com.stampcrush.backend.api.coupon.response.CouponCreateResponse;
 import com.stampcrush.backend.application.coupon.CouponService;
 import com.stampcrush.backend.application.coupon.dto.CafeCustomersResultDto;
 import com.stampcrush.backend.application.coupon.dto.CustomerUsingCouponResultDto;
+import com.stampcrush.backend.application.coupon.dto.StampCreateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +28,18 @@ public class CouponApiController {
     @GetMapping("/customers/{customerId}/coupons")
     public ResponseEntity<List<CustomerUsingCouponResultDto>> findCustomerUsingCouponByCafe(@PathVariable Long customerId, @RequestParam Long cafeId, @RequestParam boolean active) {
         return ResponseEntity.ok(couponService.findUsingCoupon(cafeId, customerId));
+    }
+
+    @PostMapping("/customers/{customerId}/coupons")
+    public ResponseEntity<CouponCreateResponse> createCoupon(@RequestBody CouponCreateRequest request, @PathVariable("customerId") Long customerId) {
+        Long couponId = couponService.createCoupon(request.getCafeId(), customerId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CouponCreateResponse(couponId));
+    }
+
+    @PostMapping("/customers/{customerId}/coupons/{couponId}/stamps")
+    public ResponseEntity<Void> createStamp(@PathVariable Long customerId, @PathVariable Long couponId, @RequestBody StampCreateRequest stampCreateRequest) {
+        StampCreateDto stampCreateDto = new StampCreateDto(1L, customerId, couponId, stampCreateRequest.getEarningStampCount());
+        couponService.createStamp(stampCreateDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
