75a860fd48c38d514819ab8284855cc24959eb7f
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-07-19 17:01:53 +0900

    [BE] fix: SampleCouponFindRequest 생성자 에러 수정 (#150)

--- backend/src/main/java/com/stampcrush/backend/api/sample/request/SampleCouponFindRequest.java
+++ backend/src/main/java/com/stampcrush/backend/api/sample/request/SampleCouponFindRequest.java
@@ -2,14 +2,17 @@ package com.stampcrush.backend.api.sample.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-@NoArgsConstructor
 public class SampleCouponFindRequest {
 
     @JsonProperty("max-stamp-count")
     private Integer maxStampCount;
+
+    public SampleCouponFindRequest() {
+    }
+
+    public SampleCouponFindRequest(Integer maxStampCount) {
+        this.maxStampCount = maxStampCount;
+    }
 }
