bc7381dfba8e58d7c82fefd722aa7d18e4671b67
regularPark <srk99279@naver.com>
2023-07-19 19:37:31 +0900

    refactor: 라우팅 변경에 따른 사이드바 value 변경

--- frontend/src/components/SideBar/PageSideBar.tsx
+++ frontend/src/components/SideBar/PageSideBar.tsx
@@ -5,7 +5,7 @@ const SIDE_BAR_OPTIONS: SideBarOptions[] = [
   { key: '내 카페 관리', value: '/admin/manage-cafe' },
   { key: '내 고객 목록', value: '/admin' },
   { key: '쿠폰 정책 변경', value: '/admin/modify-coupon-policy/1' },
-  { key: '스탬프 적립', value: '/admin/earn-stamp' },
+  { key: '스탬프 적립', value: '/admin/stamp' },
   { key: '리워드 사용', value: '/admin/use-reward' },
 ];
 
