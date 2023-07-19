2125fc196cb96ff1c713e217d706b2e3d78fdc73
gitchan <eunkeeeee@gmail.com>
2023-07-19 16:55:49 +0900

    feat: max-stamp-count 쿼리 파라미터 바인딩 설정

--- backend/src/main/java/com/stampcrush/backend/api/sample/request/SampleCouponFindRequest.java
+++ backend/src/main/java/com/stampcrush/backend/api/sample/request/SampleCouponFindRequest.java
@@ -1,11 +1,15 @@
 package com.stampcrush.backend.api.sample.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class SampleCouponFindRequest {
 
-    private final Integer maxStampCount;
+    @JsonProperty("max-stamp-count")
+    private Integer maxStampCount;
 }
