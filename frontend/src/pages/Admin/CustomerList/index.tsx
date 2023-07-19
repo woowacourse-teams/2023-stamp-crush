600049a185d97c06026d71a76f669d011384e073
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-19 16:13:19 +0900

    feat: 내 고객 목록 페이지 정렬, 검색기능 구현

--- frontend/src/pages/Admin/CustomerList/index.tsx
+++ frontend/src/pages/Admin/CustomerList/index.tsx
@@ -1,55 +1,137 @@
-import { Badge, CustomerBox, CustomerContainer, LeftInfo, Name, RightInfo } from './style';
+import {
+  Badge,
+  CustomerBox,
+  CustomerContainer,
+  LeftInfo,
+  Name,
+  RightInfo,
+  Container,
+} from './style';
 import Text from '../../../components/Text';
+import { useEffect, useState } from 'react';
+import SearchBar from '../../../components/SearchBar';
+import { useQuery } from '@tanstack/react-query';
+import SelectBox from '../../../components/SelectBox';
+
+const CUSTOMERS_ORDER_OPTIONS = [
+  {
+    key: 'stampCount',
+    value: '스탬프순',
+  },
+  {
+    key: 'rewardCount',
+    value: '리워드순',
+  },
+  {
+    key: 'firstVisitDate',
+    value: '방문일순',
+  },
+  {
+    key: 'visitCount',
+    value: '방문횟수순',
+  },
+];
+
+interface CustomerType {
+  id: number;
+  nickname: string;
+  stampCount: number;
+  maxStampCount: number;
+  rewardCount: number;
+  visitCount: number;
+  firstVisitDate: string;
+  isRegistered: boolean;
+}
+
+const getList = async () => {
+  const data = await fetch('/cafes/1/customers');
+  return await data.json();
+};
 
 const CustomerList = () => {
+  const [searchWord, setSearchWord] = useState('');
+  const [orderOption, setOrderOption] = useState({ key: 'stampCount', value: '스탬프순' });
+  const [customers, setCustomers] = useState<CustomerType[]>([]);
+  const { isLoading, isError, data, isSuccess } = useQuery(['customers'], getList, {
+    onSuccess: (data) => {
+      setCustomers(data.customers);
+      orderCustomer();
+    },
+  });
+
+  const orderCustomer = () => {
+    if (!isSuccess) return;
+    const customers = data.customers;
+
+    switch (orderOption.key) {
+      case 'firstVisitDate':
+        // TODO: 방문일 순 로직 구현
+        break;
+      default:
+        customers.sort((a: any, b: any) => (a[orderOption.key] < b[orderOption.key] ? 1 : -1));
+        setCustomers([...customers]);
+        break;
+    }
+  };
+
+  const searchCustomer = () => {
+    if (searchWord === '') return;
+    setCustomers([
+      ...data.customers.filter(
+        (customer: CustomerType) => customer.nickname === searchWord && customer,
+      ),
+    ]);
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      orderCustomer();
+    }
+  }, [orderOption]);
+
+  if (isLoading) return <CustomerContainer>Loading</CustomerContainer>;
+  if (isError) return <CustomerContainer>Error</CustomerContainer>;
+
   return (
     <CustomerContainer>
       <Text variant="pageTitle">내 고객 목록</Text>
-      <CustomerBox>
-        <LeftInfo>
-          <Name>
-            <h1>윤생</h1> <Badge $isRegistered={false}>임시</Badge>
-          </Name>
-          <span>
-            스탬프: 3/8 <br />
-            리워드: 2개
-          </span>
-        </LeftInfo>
-        <RightInfo>
-          <span>첫 방문일: 2023년 5월 6일</span>
-          <span>방문 횟수: 13번</span>
-        </RightInfo>
-      </CustomerBox>
-      <CustomerBox>
-        <LeftInfo>
-          <Name>
-            <h1>라잇</h1> <Badge $isRegistered={true}>회원</Badge>
-          </Name>
-          <span>
-            스탬프: 3/8 <br />
-            리워드: 2개
-          </span>
-        </LeftInfo>
-        <RightInfo>
-          <span>첫 방문일: 2023년 5월 6일</span>
-          <span>방문 횟수: 15번</span>
-        </RightInfo>
-      </CustomerBox>
-      <CustomerBox>
-        <LeftInfo>
-          <Name>
-            <h1>레고</h1> <Badge $isRegistered={true}>회원</Badge>
-          </Name>
-          <span>
-            스탬프: 3/8 <br />
-            리워드: 2개
-          </span>
-        </LeftInfo>
-        <RightInfo>
-          <span>첫 방문일: 2023년 5월 6일</span>
-          <span>방문 횟수: 15번</span>
-        </RightInfo>
-      </CustomerBox>
+      <Container>
+        <SearchBar searchWord={searchWord} setSearchWord={setSearchWord} onClick={searchCustomer} />
+        <SelectBox
+          options={CUSTOMERS_ORDER_OPTIONS}
+          checkedOption={orderOption}
+          setCheckedOption={setOrderOption}
+        />
+      </Container>
+      {customers.map(
+        ({
+          id,
+          nickname,
+          stampCount,
+          maxStampCount,
+          rewardCount,
+          isRegistered,
+          firstVisitDate,
+          visitCount,
+        }: CustomerType) => (
+          <CustomerBox key={id}>
+            <LeftInfo>
+              <Name>
+                <h1>{nickname}</h1>{' '}
+                <Badge $isRegistered={isRegistered}>{isRegistered ? '회원' : '임시'}</Badge>
+              </Name>
+              <span>
+                스탬프: {stampCount}/{maxStampCount} <br />
+                리워드: {rewardCount}개
+              </span>
+            </LeftInfo>
+            <RightInfo>
+              <span>첫 방문일: {firstVisitDate}</span>
+              <span>방문 횟수: {visitCount}번</span>
+            </RightInfo>
+          </CustomerBox>
+        ),
+      )}
     </CustomerContainer>
   );
 };
