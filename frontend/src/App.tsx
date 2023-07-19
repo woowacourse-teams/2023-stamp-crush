808aa2dad44a38524e34d48e5d6a10463725ffb0
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-07-19 11:34:50 +0900

    [FE] feat: react-query 보일러 플레이팅 (#140)
    * chore: react-query 설치
    
    * feat: react-query 보일러 플레이팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,20 @@ import GlobalStyle from './style/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import { theme } from './style/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
