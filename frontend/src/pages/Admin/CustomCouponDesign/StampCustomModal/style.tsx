1857ac63842f58dcc64967cf496a30b3fab9dbd4
regularPark <srk99279@naver.com>
2023-07-25 23:45:05 +0900

    design: 스탬프 커스텀 모달 스타일 수정

--- frontend/src/pages/Admin/CustomCouponDesign/StampCustomModal/style.tsx
+++ frontend/src/pages/Admin/CustomCouponDesign/StampCustomModal/style.tsx
@@ -10,6 +10,7 @@ export const BackCouponWrapper = styled.div`
   height: 300px;
   border: 1px solid black;
   box-sizing: border-box;
+  background-color: transparent;
 `;
 
 export const Stamp = styled.span<StampProps>`
@@ -21,13 +22,14 @@ export const Stamp = styled.span<StampProps>`
   top: ${(props) => props.Y}px;
   width: 50px;
   height: 50px;
+  transform: translate(-50%, -50%);
 `;
 
 export const StampBadge = styled.span`
   display: flex;
   position: absolute;
-  top: 0;
-  right: 0;
+  top: -10px;
+  right: -10px;
   justify-content: center;
   width: 20px;
   height: 20px;
@@ -35,4 +37,17 @@ export const StampBadge = styled.span`
   border: 1px solid black;
   border-radius: 50%;
   line-height: 20px;
+`;
+
+export const BackImage = styled.img`
+  width: 540px;
+  height: 300px;
+  z-index: -1;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 `;
