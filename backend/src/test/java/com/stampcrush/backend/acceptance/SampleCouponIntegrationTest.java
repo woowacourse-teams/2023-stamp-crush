137a65b121fdcf969f98f323c562c5b913e00e2c
gitchan <eunkeeeee@gmail.com>
2023-07-25 17:32:26 +0900

    refactor: 공통 메서드 BeforeEach로 분리

--- backend/src/test/java/com/stampcrush/backend/acceptance/SampleCouponIntegrationTest.java
+++ backend/src/test/java/com/stampcrush/backend/acceptance/SampleCouponIntegrationTest.java
@@ -1,9 +1,6 @@
 package com.stampcrush.backend.acceptance;
 
 import com.stampcrush.backend.entity.sample.SampleBackImage;
-import com.stampcrush.backend.entity.sample.SampleFrontImage;
-import com.stampcrush.backend.entity.sample.SampleStampCoordinate;
-import com.stampcrush.backend.entity.sample.SampleStampImage;
 import com.stampcrush.backend.repository.sample.SampleBackImageRepository;
 import com.stampcrush.backend.repository.sample.SampleFrontImageRepository;
 import com.stampcrush.backend.repository.sample.SampleStampCoordinateRepository;
@@ -11,12 +8,11 @@ import com.stampcrush.backend.repository.sample.SampleStampImageRepository;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
 
 import static com.stampcrush.backend.fixture.SampleCouponFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -36,27 +32,26 @@ public class SampleCouponIntegrationTest extends AcceptanceTest {
     @Autowired
     private SampleStampImageRepository sampleStampImageRepository;
 
-    private void saveSampleCoupon() {
-        SampleFrontImage savedSampleFrontImage = sampleFrontImageRepository.save(SAMPLE_FRONT_IMAGE);
+    @BeforeEach
+    void setUp() {
+        sampleFrontImageRepository.save(SAMPLE_FRONT_IMAGE);
         SampleBackImage savedSampleBackImage = sampleBackImageRepository.save(SAMPLE_BACK_IMAGE);
-        List<SampleStampCoordinate> savedSampleStampCoordinates = SAMPLE_COORDINATES_SIZE_EIGHT.stream()
+        SAMPLE_COORDINATES_SIZE_EIGHT.stream()
                 .peek(e -> e.setSampleBackImage(savedSampleBackImage))
                 .map(e -> sampleStampCoordinateRepository.save(e))
                 .toList();
-        SampleStampImage savedSampleStampImage = sampleStampImageRepository.save(SAMPLE_STAMP_IMAGE);
+        sampleStampImageRepository.save(SAMPLE_STAMP_IMAGE);
     }
 
     @Disabled
     @Test
     void 전체_쿠폰_샘플을_조회한다() {
         // given, when
-        saveSampleCoupon();
-
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
                 .when()
-                .get("/coupon-samples")
+                .get("/api/coupon-samples")
 
                 .then()
                 .extract();
@@ -73,8 +68,6 @@ public class SampleCouponIntegrationTest extends AcceptanceTest {
     @Test
     void 최대_스탬프_개수로_쿠폰_샘플을_필터링해서_조회한다() {
         // given, when
-        saveSampleCoupon();
-
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
@@ -96,8 +89,6 @@ public class SampleCouponIntegrationTest extends AcceptanceTest {
     @Test
     void 해당하는_최대_스탬프_개수의_샘플_쿠폰이_없으면_뒷면이_조회되지_않는다() {
         // given, when
-        saveSampleCoupon();
-
         ExtractableResponse<Response> response = RestAssured.given()
                 .log().all()
 
