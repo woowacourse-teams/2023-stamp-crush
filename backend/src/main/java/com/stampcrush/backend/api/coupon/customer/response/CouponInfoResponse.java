77297f9ca475dedf1a33e9c0d53360f91df8913c
gitchan <eunkeeeee@gmail.com>
2023-07-26 17:03:02 +0900

    feat: 쿠폰 리스트 조회 Response DTO 생성

---
+++ backend/src/main/java/com/stampcrush/backend/api/coupon/customer/response/CouponInfoResponse.java
@@ -0,0 +1,32 @@
+package com.stampcrush.backend.api.coupon.customer.response;
+
+import com.stampcrush.backend.entity.coupon.CouponStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class CouponInfoResponse {
+
+    private final Long id;
+    private final Boolean isFavorites;
+    private final CouponStatus status;
+    private final Integer stampCount;
+    private final Integer maxStampCount;
+    private final String rewardName;
+    private final String frontImageUrl;
+    private final String backImageUrl;
+    private final String stampImageUrl;
+    private final List<CoordinatesResponse> coordinates;
+
+    @Getter
+    @RequiredArgsConstructor
+    private class CoordinatesResponse {
+
+        private final Integer order;
+        private final Integer xCoordinate;
+        private final Integer yCoordinate;
+    }
+}
