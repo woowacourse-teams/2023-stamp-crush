6f80a0a91799698bec7f5ca04484394c4304f398
yenawee <yaena0319@naver.com>
2023-07-26 10:26:18 +0900

    help: swagger ui 문서 생성 안됨

--- backend/src/test/java/com/stampcrush/backend/api/customer/CustomerApiControllerTest.java
+++ backend/src/test/java/com/stampcrush/backend/api/customer/CustomerApiControllerTest.java
@@ -1,31 +1,26 @@
 package com.stampcrush.backend.api.customer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.stampcrush.backend.api.BaseControllerTest;
 import com.stampcrush.backend.api.customer.request.TemporaryCustomerCreateRequest;
 import com.stampcrush.backend.application.customer.CustomerService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 
 @WebMvcTest(CustomerApiController.class)
-@AutoConfigureMockMvc
 @MockBean(JpaMetamodelMappingContext.class)
-class CustomerApiControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+class CustomerApiControllerTest extends BaseControllerTest {
 
     @MockBean
     private CustomerService customerService;
@@ -38,12 +33,22 @@ class CustomerApiControllerTest {
         when(customerService.createTemporaryCustomer(request.getPhoneNumber()))
                 .thenReturn(customerId);
 
-        mockMvc.perform(
-                        MockMvcRequestBuilders.post("/api/temporary-customers")
-                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        ResultActions resultActions = mockMvc.perform(
+                        post("/api/temporary-customers")
+                                .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(request))
-                )
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.header().string("Location", "/customers/" + customerId));
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document("customer docs",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                resource(
+                                        ResourceSnippetParameters.builder()
+                                                .description("임시고객 생성")
+                                                .requestFields(fieldWithPath("phoneNumber").description("전화번호"))
+                                                .build()
+                                )
+                        )
+                );
     }
 }
