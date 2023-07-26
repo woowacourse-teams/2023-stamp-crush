87f438aa7ef7ef287df7a7cf05464328542775ce
gitchan <eunkeeeee@gmail.com>
2023-07-26 17:27:25 +0900

    test: 카페 생성 요청 step 생성

--- backend/src/test/java/com/stampcrush/backend/acceptance/CouponFindAcceptanceTest.java
+++ backend/src/test/java/com/stampcrush/backend/acceptance/CouponFindAcceptanceTest.java
@@ -1,11 +1,13 @@
 package com.stampcrush.backend.acceptance;
 
+import com.stampcrush.backend.api.cafe.request.CafeCreateRequest;
 import com.stampcrush.backend.api.customer.request.TemporaryCustomerCreateRequest;
 import com.stampcrush.backend.entity.user.Owner;
 import com.stampcrush.backend.repository.user.OwnerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static com.stampcrush.backend.acceptance.step.CafeCreateStep.카페_생성_요청하고_아이디_반환;
 import static com.stampcrush.backend.acceptance.step.TemporaryCustomerCreateStep.전화번호로_임시_고객_등록_요청하고_아이디_반환;
 import static com.stampcrush.backend.fixture.OwnerFixture.GITCHAN;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,6 +23,9 @@ public class CouponFindAcceptanceTest extends AcceptanceTest {
         Owner savedOwner = ownerRepository.save(GITCHAN);
         assertThat(savedOwner.getId()).isNotNull();
         assertThat(savedOwner.getName()).isEqualTo(GITCHAN.getName());
+
+        CafeCreateRequest cafeCreateRequest = new CafeCreateRequest("깃짱카페", "서초구", "우리집", "01010101010");
+        Long cafeId = 카페_생성_요청하고_아이디_반환(savedOwner.getId(), cafeCreateRequest);
 
         Long customerId = 전화번호로_임시_고객_등록_요청하고_아이디_반환(new TemporaryCustomerCreateRequest("01012345678"));
         assertThat(customerId).isNotNull();
