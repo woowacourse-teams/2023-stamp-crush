b8bff0ee3f2d1b0f321c9a5685291acd6475b52e
yenawee <yaena0319@naver.com>
2023-07-26 10:26:18 +0900

    help: swagger ui 문서 생성 안됨

--- backend/src/test/java/com/stampcrush/backend/api/BaseControllerTest.java
+++ backend/src/test/java/com/stampcrush/backend/api/BaseControllerTest.java
@@ -1,43 +1,39 @@
-//package com.stampcrush.backend.api;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.builder.RequestSpecBuilder;
-//import io.restassured.specification.RequestSpecification;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//import org.springframework.restdocs.RestDocumentationContextProvider;
-//import org.springframework.restdocs.RestDocumentationExtension;
-//
-//import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-//import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-//import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.documentationConfiguration;
-//
-//@SpringBootTest(webEnvironment = RANDOM_PORT)
-//@ExtendWith(RestDocumentationExtension.class)
-//public class BaseControllerTest {
-//
-//    protected static final String DEFAULT_RESTDOC_PATH = "{class_name}/{method_name}/";
-//
-//    protected RequestSpecification spec;
-//
-//    @LocalServerPort
-//    int port;
-//
-//    @BeforeEach
-//    void setUp() {
-//        RestAssured.port = port;
-//    }
-//
-//    @BeforeEach
-//    void setUpRestDocs(RestDocumentationContextProvider restDocumentation) {
-//        this.spec = new RequestSpecBuilder()
-//                .setPort(port)
-//                .addFilter(documentationConfiguration(restDocumentation)
-//                        .operationPreprocessors()
-//                        .withRequestDefaults(prettyPrint())
-//                        .withResponseDefaults(prettyPrint()))
-//                .build();
-//    }
-//}
+package com.stampcrush.backend.api;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ExtendWith({RestDocumentationExtension.class})
+public abstract class BaseControllerTest {
+
+    @Autowired
+    protected WebApplicationContext ctx;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(final RestDocumentationContextProvider restDocumentation) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(print())
+                .build();
+    }
+}
