c4cfc5afd9643acd029f051654c2c19fd2c9e832
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-07-26 15:27:39 +0900

    feat: swagger 사용해서 API 자동화 추가 (#226)

---
+++ backend/src/main/java/com/stampcrush/backend/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.stampcrush.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+}
