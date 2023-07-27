8676f7424e1aba512ed98fed263a56436840f537
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-27 20:44:18 +0900

    feat: Alert Component 구현

---
+++ frontend/src/components/Alert/index.tsx
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom';
+import { AlertContainer, BackDrop, OptionContainer, OptionWrapper, TextContainer } from './style';
+import { CiCircleAlert } from 'react-icons/ci';
+
+interface AlertProps {
+  text: string;
+  rightOption: string;
+  leftOption: string;
+  onClickRight: () => void;
+  onClickLeft: () => void;
+}
+
+const Alert = ({ text, rightOption, leftOption, onClickLeft, onClickRight }: AlertProps) => {
+  return ReactDOM.createPortal(
+    <>
+      <BackDrop onClick={onClickLeft} />
+      <AlertContainer>
+        <TextContainer>
+          <CiCircleAlert size={60} color={'#6e6e6e'} />
+          {text}
+        </TextContainer>
+        <OptionContainer>
+          <OptionWrapper $option={'left'} onClick={onClickLeft}>
+            {leftOption}
+          </OptionWrapper>
+          <OptionWrapper $option={'right'} onClick={onClickRight}>
+            {rightOption}
+          </OptionWrapper>
+        </OptionContainer>
+      </AlertContainer>
+    </>,
+    document.querySelector('body') as HTMLBodyElement,
+  );
+};
+
+export default Alert;
