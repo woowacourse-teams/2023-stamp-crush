3c23db9544a1b03ff626ceaaae9ef4b2fe8a6357
kangyeongmin <kym6050@hanyang.ac.kr>
2023-07-27 13:27:52 +0900

    refactor: selectBox 너비를 외부에서 받도록 수정

--- frontend/src/components/ProgressBar/style.tsx
+++ frontend/src/components/ProgressBar/style.tsx
@@ -6,14 +6,14 @@ type ProgressProps = {
 
 export const Bar = styled.div`
   width: 80%;
-  height: 10px;
+  height: 12px;
   background-color: #ddd;
   border-radius: 8px;
 `;
 
 export const Progress = styled.div<ProgressProps>`
   width: ${(props) => `${props.$width}%`};
-  height: 10px;
+  height: 12px;
   background-color: skyblue;
   border-radius: 8px;
   transition: all 0.4s;
