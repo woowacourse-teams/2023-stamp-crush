1ddfa17d795b0ef4bf5aff1315c7c29bc3aa7f77
regularPark <srk99279@naver.com>
2023-07-27 14:05:06 +0900

    feat: 전화번호에 `-`을 적절히 삽입하는 유틸 메서드 작성

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,5 @@
+import { Time } from '../pages/Admin/ManageCafe';
+
 export const formatDate = (dateString: string) => {
   const dateArray = dateString.split(':');
 
@@ -14,4 +16,29 @@ export const formatDate = (dateString: string) => {
 
 export const parseStampCount = (value: string) => {
   return +value.replaceAll('개', '');
+};
+
+export const parseTime = (value: Time) => {
+  return value.hour + ':' + value.minute;
+};
+
+export const parsePhoneNumber = (phoneNumber: string | undefined) => {
+  if (!phoneNumber) return;
+  const number = phoneNumber.replace(/\D/g, ''); // Remove all non-numeric characters
+
+  if (number.startsWith('02')) {
+    return number.length <= 2
+      ? number
+      : number.length <= 6
+      ? number.substring(0, 2) + '-' + number.substring(2)
+      : number.length <= 9
+      ? number.substring(0, 2) + '-' + number.substring(2, 5) + '-' + number.substring(5)
+      : number.substring(0, 2) + '-' + number.substring(2, 6) + '-' + number.substring(6, 10);
+  } else {
+    return number.length < 4
+      ? number
+      : number.length < 7
+      ? number.substring(0, 3) + '-' + number.substring(3)
+      : number.substring(0, 3) + '-' + number.substring(3, 7) + '-' + number.substring(7, 11);
+  }
 };
