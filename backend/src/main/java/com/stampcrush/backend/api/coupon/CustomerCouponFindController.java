923dd39d45b4945320af0cc7b2ce133d7f4d0ad5
gitchan <eunkeeeee@gmail.com>
2023-07-27 17:33:29 +0900

    feat: 컨트롤러 반환값 변경

--- backend/src/main/java/com/stampcrush/backend/api/coupon/CustomerCouponFindController.java
+++ backend/src/main/java/com/stampcrush/backend/api/coupon/CustomerCouponFindController.java
@@ -4,6 +4,7 @@ import com.stampcrush.backend.api.coupon.response.CustomerCouponsFindResponse;
 import com.stampcrush.backend.application.coupon.CustomerCouponFindService;
 import com.stampcrush.backend.application.coupon.dto.CustomerCouponFindResultDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class CustomerCouponFindController {
     private final CustomerCouponFindService customerCouponFindService;
 
     @GetMapping("/coupons/{customerId}") // customerId는 이후 제거 예정
-    public CustomerCouponsFindResponse findOneCouponForOneCafe(@PathVariable("customerId") Long customerId) {
+    public ResponseEntity<CustomerCouponsFindResponse> findOneCouponForOneCafe(@PathVariable("customerId") Long customerId) {
         List<CustomerCouponFindResultDto> coupons = customerCouponFindService.findOneCouponForOneCafe(customerId);
-        return CustomerCouponsFindResponse.from(coupons);
+        return ResponseEntity.ok().body(CustomerCouponsFindResponse.from(coupons));
     }
 }
