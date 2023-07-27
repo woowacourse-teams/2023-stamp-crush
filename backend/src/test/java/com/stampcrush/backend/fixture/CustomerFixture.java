394380c2bc45cb846aa94c560f4d15de1afefb23
gitchan <eunkeeeee@gmail.com>
2023-07-27 10:45:55 +0900

    fix: 세미콜론 추가

--- backend/src/test/java/com/stampcrush/backend/fixture/CustomerFixture.java
+++ backend/src/test/java/com/stampcrush/backend/fixture/CustomerFixture.java
@@ -10,7 +10,7 @@ public class CustomerFixture {
     public static final TemporaryCustomer TEMPORARY_CUSTOMER_3 = TemporaryCustomer.from("깃짱 번호");
     public static final RegisterCustomer REGISTER_CUSTOMER_1 = new RegisterCustomer("깃짱 닉네임", "깃짱 번호", "깃짱 아이디", "깃짱 비번");
     public static final RegisterCustomer REGISTER_CUSTOMER_2 = new RegisterCustomer("깃짱 닉네임", "깃짱 번호", "깃짱 아이디", "깃짱 비번");
-    public static final RegisterCustomer REGISTER_CUSTOMER_GITCHAN = new RegisterCustomer("깃짱", "01012345678", "gitchan", "password")
+    public static final RegisterCustomer REGISTER_CUSTOMER_GITCHAN = new RegisterCustomer("깃짱", "01012345678", "gitchan", "password");
 
     private CustomerFixture() {
     }
