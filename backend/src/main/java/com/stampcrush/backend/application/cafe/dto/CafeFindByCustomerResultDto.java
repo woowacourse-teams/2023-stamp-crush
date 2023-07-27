659b839f11311779c6a477d2ff3b3e53931606b5
yenawee <yaena0319@naver.com>
2023-07-27 15:56:59 +0900

    feat: 고객모드에서 카페 정보 조회하는 API 구현

---
+++ backend/src/main/java/com/stampcrush/backend/application/cafe/dto/CafeFindByCustomerResultDto.java
@@ -0,0 +1,40 @@
+package com.stampcrush.backend.application.cafe.dto;
+
+import com.stampcrush.backend.entity.cafe.Cafe;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+
+@Getter
+@RequiredArgsConstructor
+public class CafeFindByCustomerResultDto {
+
+    private final Long id;
+    private final String name;
+//  private final String introduction;
+
+    @DateTimeFormat(pattern = "HH:mm")
+    private final LocalTime openTime;
+
+    @DateTimeFormat(pattern = "HH:mm")
+    private final LocalTime closeTime;
+
+    private final String telephoneNumber;
+    private final String cafeImageUrl;
+    private final String roadAddress;
+    private final String detailAddress;
+
+    public static CafeFindByCustomerResultDto from(Cafe cafe) {
+        return new CafeFindByCustomerResultDto(
+                cafe.getId(),
+                cafe.getName(),
+                cafe.getOpenTime(),
+                cafe.getCloseTime(),
+                cafe.getTelephoneNumber(),
+                cafe.getCafeImageUrl(),
+                cafe.getRoadAddress(),
+                cafe.getDetailAddress());
+    }
+}
