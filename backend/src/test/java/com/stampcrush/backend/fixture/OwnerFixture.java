20c4ad143e57980f42b5ab79e767d2276af78777
gitchan <eunkeeeee@gmail.com>
2023-07-27 13:18:23 +0900

    feat: 쿠폰을 고객과 상태에 따라 필터링

--- backend/src/test/java/com/stampcrush/backend/fixture/OwnerFixture.java
+++ backend/src/test/java/com/stampcrush/backend/fixture/OwnerFixture.java
@@ -6,6 +6,7 @@ public class OwnerFixture {
     public static final Owner OWNER1 = new Owner("이름", "아이디", "비번", "번호");
     public static final Owner OWNER2 = new Owner("이름", "아이디", "비번", "번호");
     public static final Owner GITCHAN = new Owner("깃짱", "gitchan", "password", "01011112222");
+    public static final Owner JENA = new Owner("제나", "jena", "password", "0101010");
 
     private OwnerFixture() {
     }
