d4c7867dda0b0d199ba3e22d4a955d5ce07210d8
gitchan <eunkeeeee@gmail.com>
2023-07-28 16:19:31 +0900

    refactor: PersistenceConfig 추가

---
+++ backend/src/main/java/com/stampcrush/backend/config/PersistenceConfig.java
@@ -0,0 +1,9 @@
+package com.stampcrush.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+}
