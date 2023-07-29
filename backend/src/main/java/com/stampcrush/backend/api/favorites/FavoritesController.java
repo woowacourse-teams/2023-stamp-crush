634cd37bf7f7288f95db316a8fee2f3b38580a24
JunDonghyuk <ehdgur4814@naver.com>
2023-07-29 22:53:45 +0900

    feat: FavoritesController 즐겨찾기 요청 반환타입 변경

--- backend/src/main/java/com/stampcrush/backend/api/favorites/FavoritesController.java
+++ backend/src/main/java/com/stampcrush/backend/api/favorites/FavoritesController.java
@@ -5,6 +5,7 @@ import com.stampcrush.backend.application.favorites.FavoritesService;
 import com.stampcrush.backend.entity.user.Customer;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,7 +16,8 @@ public class FavoritesController {
     public final FavoritesService favoritesService;
 
     @PostMapping("/cafes/{cafeId}/favorites")
-    public void updateFavorites(Customer customer, @PathVariable Long cafeId, @Valid @RequestBody FavoritesUpdateRequest favoritesUpdateRequest) {
+    public ResponseEntity<Void> updateFavorites(Customer customer, @PathVariable Long cafeId, @Valid @RequestBody FavoritesUpdateRequest favoritesUpdateRequest) {
         favoritesService.changeFavorites(customer, cafeId, favoritesUpdateRequest.getIsFavorites());
+        return ResponseEntity.ok().build();
     }
 }
