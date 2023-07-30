0464f06a7134665ca76db4b873355b1566fc3423
regularPark <srk99279@naver.com>
2023-07-30 16:39:34 +0900

    refactor: 재사용하는 type 모아둔 types 생성

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,39 @@
+export interface StampCoordinate {
+  order: number;
+  xCoordinate: number;
+  yCoordinate: number;
+}
+
+export interface CafeType {
+  cafe: {
+    id: number;
+    name: string;
+    introduction: string;
+    openTime: string;
+    closeTime: string;
+    telephoneNumber: string;
+    cafeImageUrl: string;
+    roadAddress: string;
+    detailAddress: string;
+  };
+}
+
+export interface CouponType {
+  cafeInfo: {
+    id: number;
+    name: string;
+  };
+  couponInfos: [
+    {
+      id: number;
+      isFavorites: boolean;
+      stampCount: number;
+      maxStampCount: number;
+      rewardName: string;
+      frontImageUrl: string;
+      backImageUrl: string;
+      stampImageUrl: string;
+      coordinates: StampCoordinate[];
+    },
+  ];
+}
