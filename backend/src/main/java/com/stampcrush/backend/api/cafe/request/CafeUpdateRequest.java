648c98871868d349ec5500f7f6f15c6e1d1cfd56
Youngho <cyh6099@gmail.com>
2023-07-31 14:46:26 +0900

    refactor: dto validation어노테이션 수정

--- backend/src/main/java/com/stampcrush/backend/api/cafe/request/CafeUpdateRequest.java
+++ backend/src/main/java/com/stampcrush/backend/api/cafe/request/CafeUpdateRequest.java
@@ -1,7 +1,5 @@
 package com.stampcrush.backend.api.cafe.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,19 +13,11 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 public class CafeUpdateRequest {
 
-    @NotNull
-    @NotBlank
     private LocalTime openTime;
 
-    @NotNull
-    @NotBlank
     private LocalTime closeTime;
 
-    @NotNull
-    @NotBlank
     private String telephoneNumber;
 
-    @NotNull
-    @NotBlank
     private String cafeImageUrl;
 }
