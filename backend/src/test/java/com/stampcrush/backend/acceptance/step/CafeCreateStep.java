2a240e97b3ae2753b6bf7487c4fa282884d8f961
yenawee <yaena0319@naver.com>
2023-07-31 18:19:56 +0900

    feat: 헤더 인증 추가로 실패하는 인수 테스트에 basic header 인증 추가

--- backend/src/test/java/com/stampcrush/backend/acceptance/step/CafeCreateStep.java
+++ backend/src/test/java/com/stampcrush/backend/acceptance/step/CafeCreateStep.java
@@ -1,6 +1,7 @@
 package com.stampcrush.backend.acceptance.step;
 
 import com.stampcrush.backend.api.cafe.request.CafeCreateRequest;
+import com.stampcrush.backend.entity.user.Owner;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -11,18 +12,19 @@ public class CafeCreateStep {
 
     public static final CafeCreateRequest CAFE_CREATE_REQUEST = new CafeCreateRequest("깃짱카페", "서초구", "우리집", "01010101010");
 
-    public static Long 카페_생성_요청하고_아이디_반환(Long ownerId, CafeCreateRequest cafeCreateRequest) {
-        ExtractableResponse<Response> response = 카페_생성_요청(ownerId, cafeCreateRequest);
+    public static Long 카페_생성_요청하고_아이디_반환(Owner owner, Long ownerId, CafeCreateRequest cafeCreateRequest) {
+        ExtractableResponse<Response> response = 카페_생성_요청(owner, ownerId, cafeCreateRequest);
         String location = response.header("Location");
         System.out.println("location = " + location);
         return Long.valueOf(location.split("/")[2]);
     }
 
-    public static ExtractableResponse<Response> 카페_생성_요청(Long ownerId, CafeCreateRequest cafeCreateRequest) {
+    public static ExtractableResponse<Response> 카페_생성_요청(Owner owner, Long ownerId, CafeCreateRequest cafeCreateRequest) {
         return RestAssured.given()
                 .log().all()
                 .contentType(JSON)
                 .body(cafeCreateRequest)
+                .auth().preemptive().basic(owner.getLoginId(), owner.getEncryptedPassword())
 
                 .when()
                 .post("/api/admin/cafes/" + ownerId)
--- backend/src/test/java/com/stampcrush/backend/acceptance/step/CouponFindStep.java
+++ backend/src/test/java/com/stampcrush/backend/acceptance/step/CouponFindStep.java
@@ -1,14 +1,16 @@
 package com.stampcrush.backend.acceptance.step;
 
+import com.stampcrush.backend.entity.user.Owner;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
 public class CouponFindStep {
 
-    public static ExtractableResponse<Response> 고객의_쿠폰_카페별로_1개씩_조회_요청(Long customerId) {
+    public static ExtractableResponse<Response> 고객의_쿠폰_카페별로_1개씩_조회_요청(Owner owner, Long customerId) {
         return RestAssured.given()
                 .log().all()
+                .auth().preemptive().basic(owner.getLoginId(), owner.getEncryptedPassword())
 
                 .when()
                 .get("/api/coupons/" + customerId)
