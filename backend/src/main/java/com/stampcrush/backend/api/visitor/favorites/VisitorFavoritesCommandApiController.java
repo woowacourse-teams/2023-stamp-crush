eecf5a652d53b88bfed98660af5b162f204fabac
Youngho <cyh6099@gmail.com>
2023-08-01 11:44:53 +0900

    refactor: FavoritesController 네이밍 수정

--- backend/src/main/java/com/stampcrush/backend/api/visitor/favorites/VisitorFavoritesCommandApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/favorites/VisitorFavoritesCommandApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
-public class FavoritesController {
+public class VisitorFavoritesCommandApiController {
 
     public final FavoritesService favoritesService;
 
