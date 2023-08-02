d018d084fa943d63252f39249edbcceaa1b94372
kangyeongmin <kym6050@hanyang.ac.kr>
2023-08-02 15:32:07 +0900

    feat: api header 하드코딩

--- frontend/src/api/patch.ts
+++ frontend/src/api/patch.ts
@@ -1,8 +1,8 @@
-import { api } from '.';
+import { api, customerHeader, ownerHeader } from '.';
 import { CafeInfoReq } from '../types/api';
 
 export const patchReward = async (customerId: number, rewardId: number) => {
-  await api.patch(`/customers/${customerId}/rewards/${rewardId}`, {
+  await api.patch(`/customers/${customerId}/rewards/${rewardId}`, customerHeader, {
     cafeId: 1,
     used: true,
   });
@@ -12,7 +12,7 @@ export const patchCafeInfo = async (
   cafeId: number,
   { openTime, closeTime, telephoneNumber, cafeImageUrl, introduction }: CafeInfoReq,
 ) => {
-  await api.patch(`/admin/cafes/${cafeId}`, {
+  await api.patch(`/admin/cafes/${cafeId}`, ownerHeader, {
     openTime,
     closeTime,
     telephoneNumber,
