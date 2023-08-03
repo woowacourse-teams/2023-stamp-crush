dff08afa470ba045d9bccd06c8c5337a0e7f34d3
박정규 <srk99279@naver.com>
2023-08-03 18:16:40 +0900

    [FE] fix: 배포 오류 해결 (#323)

--- frontend/src/pages/CouponList/Coupon/index.tsx
+++ frontend/src/pages/CouponList/Coupon/index.tsx
@@ -4,14 +4,17 @@ import { CouponWrapper } from './style';
 interface CouponProps {
   coupon: CouponType;
   onClick: () => void;
+  isFocused: boolean;
 }
 
-const Coupon = ({ coupon, onClick }: CouponProps) => {
+const Coupon = ({ coupon, onClick, isFocused }: CouponProps) => {
   return (
     <CouponWrapper
-      src={coupon.couponInfos[0].frontImageUrl}
+      $src={coupon.couponInfos[0].frontImageUrl}
       onClick={onClick}
       aria-label={coupon.cafeInfo.name}
+      aria-hidden={isFocused ? 'false' : 'true'}
+      disabled={!isFocused}
     />
   );
 };
