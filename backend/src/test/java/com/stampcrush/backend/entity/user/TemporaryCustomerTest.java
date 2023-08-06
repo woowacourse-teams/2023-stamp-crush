9d8d758f135c845b504fe7c40a1a75fb2699ab9d
yenawee <yaena0319@naver.com>
2023-08-06 14:05:48 +0900

    feat: TemporaryCustomerTest 작성

---
+++ backend/src/test/java/com/stampcrush/backend/entity/user/TemporaryCustomerTest.java
@@ -0,0 +1,18 @@
+package com.stampcrush.backend.entity.user;
+
+import com.stampcrush.backend.exception.CustomerBadRequestException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TemporaryCustomerTest {
+
+    @Test
+    void 전화번호_길이가_4자_이하일때_닉네임_생성_안되어_에러_발생() {
+        // given
+        String invalidPhoneNumber = "010";
+
+        // when, then
+        assertThatThrownBy(() -> TemporaryCustomer.from(invalidPhoneNumber)).isInstanceOf(CustomerBadRequestException.class);
+    }
+}
