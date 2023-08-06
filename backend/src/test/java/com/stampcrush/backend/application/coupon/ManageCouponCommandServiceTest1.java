33972256d35498f32c61773ede3eb147acb42a32
Youngho <cyh6099@gmail.com>
2023-08-06 15:10:26 +0900

    test: 쿠폰 발급 예외 테스트 작성

--- backend/src/test/java/com/stampcrush/backend/application/coupon/ManageCouponCommandServiceTest1.java
+++ backend/src/test/java/com/stampcrush/backend/application/coupon/ManageCouponCommandServiceTest1.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -90,6 +91,7 @@ public class ManageCouponCommandServiceTest1 {
 
     @Test
     void 적립중인_쿠폰이_있으면_새로운_쿠폰을_발급하고_기존_쿠폰은_만료된다() {
+        // given
         Coupon currentCoupon = new Coupon(LocalDate.EPOCH, customer, cafe, null, couponPolicy);
         given(customerRepository.findById(anyLong()))
                 .willReturn(Optional.of(customer));
@@ -102,8 +104,10 @@ public class ManageCouponCommandServiceTest1 {
         given(couponRepository.findByCafeAndCustomerAndStatus(any(), any(), any()))
                 .willReturn(List.of(currentCoupon));
 
+        // when
         managerCouponCommandService.createCoupon(1L, 1L);
 
+        // then
         then(couponRepository).should(times(1)).save(any());
         then(couponDesignRepository).should(times(1)).save(any());
         then(couponPolicyRepository).should(times(1)).save(any());
@@ -112,6 +116,7 @@ public class ManageCouponCommandServiceTest1 {
 
     @Test
     void 적립중인_쿠폰이_없으면_새로운_쿠폰만_발급한다() {
+        // given
         given(customerRepository.findById(anyLong()))
                 .willReturn(Optional.of(customer));
         given(cafeRepository.findById(anyLong()))
@@ -123,11 +128,69 @@ public class ManageCouponCommandServiceTest1 {
         given(couponRepository.findByCafeAndCustomerAndStatus(any(), any(), any()))
                 .willReturn(Collections.emptyList());
 
+        // when
         managerCouponCommandService.createCoupon(1L, 1L);
 
+        // then
         then(couponRepository).should(times(1)).save(any());
         then(couponDesignRepository).should(times(1)).save(any());
         then(couponPolicyRepository).should(times(1)).save(any());
+    }
+
+    @Test
+    void 존재하지_않는_회원이_쿠폰을_발급받으려_하면_예외발생() {
+        // given, when
+        given(customerRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> managerCouponCommandService.createCoupon(1L, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 존재하지_않는_카페가_쿠폰을_발급하려고_하면_예외발생() {
+        // given, when
+        given(customerRepository.findById(anyLong()))
+                .willReturn(Optional.of(customer));
+        given(cafeRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> managerCouponCommandService.createCoupon(1L, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 쿠폰_발급_시_카페의_정책이_존재하지_않으면_예외발생() {
+        // given, when
+        given(customerRepository.findById(anyLong()))
+                .willReturn(Optional.of(customer));
+        given(cafeRepository.findById(anyLong()))
+                .willReturn(Optional.of(cafe));
+        given(cafePolicyRepository.findByCafe(any()))
+                .willReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> managerCouponCommandService.createCoupon(1L, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 쿠폰_발급_시_카페의_쿠폰_디자인이_없으면_예외발생() {
+        // given, when
+        given(customerRepository.findById(anyLong()))
+                .willReturn(Optional.of(customer));
+        given(cafeRepository.findById(anyLong()))
+                .willReturn(Optional.of(cafe));
+        given(cafePolicyRepository.findByCafe(any()))
+                .willReturn(Optional.of(new CafePolicy(10, "reward", 6, false, cafe)));
+        given(cafeCouponDesignRepository.findByCafe(any()))
+                .willReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> managerCouponCommandService.createCoupon(1L, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -195,14 +258,17 @@ public class ManageCouponCommandServiceTest1 {
 
     @Test
     void 기존에_스탬프가_있는_쿠폰에_추가_적립한_스탬프로_리워드를_받기_위한_스탬프_개수보다_적으면_스탬프만_적립한다() {
+        // given
         Coupon currentCoupon = new Coupon(LocalDate.EPOCH, customer, cafe, null, couponPolicy);
         currentCoupon.accumulate(4);
         int maxStampCount = 10;
         스탬프_적립을_위해_필요한_엔티티를_조회한다(maxStampCount, currentCoupon);
 
+        // when
         StampCreateDto stampCreateDto = new StampCreateDto(1L, 1L, 1L, 3);
         managerCouponCommandService.createStamp(stampCreateDto);
 
+        // then
         then(rewardRepository).should(times(0)).save(any());
         then(couponRepository).should(times(0)).save(any());
         assertThat(currentCoupon.getStatus()).isEqualTo(CouponStatus.ACCUMULATING);
@@ -211,15 +277,18 @@ public class ManageCouponCommandServiceTest1 {
 
     @Test
     void 기존에_스탬프가_있는_쿠폰에_추가_적립한_스탬프로_리워드를_받기_위한_스탬프_개수와_같으면_리워드를_생성한다() {
+        // givem
         Coupon currentCoupon = new Coupon(LocalDate.EPOCH, customer, cafe, null, couponPolicy);
         currentCoupon.accumulate(4);
         int maxStampCount = 10;
         스탬프_적립을_위해_필요한_엔티티를_조회한다(maxStampCount, currentCoupon);
 
+        // when
         int earningStamp = 6;
         StampCreateDto stampCreateDto = new StampCreateDto(1L, 1L, 1L, earningStamp);
         managerCouponCommandService.createStamp(stampCreateDto);
 
+        // then
         then(rewardRepository).should(times(1)).save(any());
         then(couponRepository).should(times(0)).save(any());
         assertThat(currentCoupon.getStatus()).isEqualTo(CouponStatus.REWARDED);
@@ -228,15 +297,18 @@ public class ManageCouponCommandServiceTest1 {
 
     @Test
     void 기존에_스탬프가_있는_쿠폰에_추가_적립한_스탬프로_리워드를_받기_위한_스탬프_개수를_초과하면_리워드를_생성하고_새로운_쿠폰에_나머지_스탬프가_찍힌다() {
+        // given
         Coupon currentCoupon = new Coupon(LocalDate.EPOCH, customer, cafe, null, couponPolicy);
         currentCoupon.accumulate(4);
         int maxStampCount = 10;
         스탬프_적립을_위해_필요한_엔티티를_조회한다(maxStampCount, currentCoupon);
 
+        // when
         int earningStamp = 17;
         StampCreateDto stampCreateDto = new StampCreateDto(1L, 1L, 1L, earningStamp);
         managerCouponCommandService.createStamp(stampCreateDto);
 
+        // then
         then(rewardRepository).should(times(2)).save(any());
         then(couponRepository).should(times(2)).save(any());
     }
