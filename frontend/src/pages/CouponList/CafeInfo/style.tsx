dc3444c957da16f9f863662a31d74ee6e2fa5eab
kangyeongmin <kym6050@hanyang.ac.kr>
2023-08-06 22:51:22 +0900

    fix: 홈에서 마이페이지로 갔다가 돌아왔을 때 쿠폰이 넘어가지 않던 문제 수정

---
+++ frontend/src/pages/CouponList/CafeInfo/style.tsx
@@ -0,0 +1,62 @@
+import { styled } from 'styled-components';
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 60px 30px;
+  gap: 20px;
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 36px;
+  gap: 10px;
+`;
+
+export const CafeName = styled.span`
+  font-size: 36px;
+  font-weight: 700;
+  margin-top: 5px;
+`;
+
+export const ProgressBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 24px;
+  color: #888;
+`;
+
+export const StampCount = styled.span`
+  margin-left: 10px;
+  font-size: 36px;
+  font-weight: 600;
+  color: black;
+`;
+
+export const MaxStampCount = styled.span`
+  font-size: 24px;
+  color: #f3b209;
+`;
+
+export const BackDrop = styled.div<{ $couponMainColor: string }>`
+  z-index: -10;
+  width: 100%;
+  max-width: 450px;
+  overflow: hidden;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: linear-gradient(
+    white,
+    rgba(255, 255, 255, 0.5) 32%,
+    ${(props) => props.$couponMainColor} 80%,
+    white
+  );
+  opacity: 0.7;
+  left: 50%;
+  transform: translateX(-50%);
+`;
