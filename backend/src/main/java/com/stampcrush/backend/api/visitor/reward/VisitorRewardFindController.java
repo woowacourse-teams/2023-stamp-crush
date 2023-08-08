4a648e3fa1f7e6b670eb29b093271c0731e16fd9
gitchan <eunkeeeee@gmail.com>
2023-08-08 17:49:25 +0900

    feat: VisitorRewardFindController 생성

---
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/reward/VisitorRewardFindController.java
@@ -0,0 +1,11 @@
+package com.stampcrush.backend.api.visitor.reward;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/rewards")
+public class VisitorRewardFindController {
+}
