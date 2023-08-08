1f3d2f62613307bba84b7694f4fca38995a136fe
kangyeongmin <kym6050@hanyang.ac.kr>
2023-08-08 11:00:13 +0900

    refactor: 입력을 4번의 step으로 나누어 받도록 수정

--- frontend/src/pages/Admin/ModifyCouponPolicy/index.tsx
+++ frontend/src/pages/Admin/ModifyCouponPolicy/index.tsx
@@ -1,19 +1,21 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../components/Button';
-import { Input } from '../../../components/Input';
-import { NextButtonWrapper } from './style';
+import { ButtonContainer, StepContainer, StepWrapper } from './style';
 import { Spacing } from '../../../style/layout/common';
-import SelectBox from '../../../components/SelectBox';
 import Text from '../../../components/Text';
 import { EXPIRE_DATE_OPTIONS, ROUTER_PATH, STAMP_COUNT_OPTIONS } from '../../../constants';
-import RadioInputs from './RadioInput';
-import { Option } from '../../../types';
+import { CouponCreated, Option } from '../../../types';
+import CreatedType from './CreatedType';
+import MaxStampCount from './MaxStampCount';
+import ExpiredPeriod from './ExpiredPeriod';
+import RewardName from './RewardName';
 
 const ModifyCouponPolicy = () => {
   const navigate = useNavigate();
-  const [createdType, setCreatedType] = useState('');
-  const rewardInputRef = useRef<HTMLInputElement>(null);
+  const [step, setStep] = useState(1);
+  const [createdType, setCreatedType] = useState<CouponCreated>('template');
+  const [rewardName, setRewardName] = useState('');
   const [expireSelect, setExpireSelect] = useState<Option>({
     key: EXPIRE_DATE_OPTIONS[0].key,
     value: EXPIRE_DATE_OPTIONS[0].value,
@@ -24,16 +26,11 @@ const ModifyCouponPolicy = () => {
   });
 
   const navigateNextPage = () => {
-    if (!createdType || !rewardInputRef.current?.value || !expireSelect || !stampCount) {
-      alert('모두 작성해주세요.');
-      return;
-    }
-
     if (createdType === 'template') {
       navigate(ROUTER_PATH.templateCouponDesign, {
         state: {
           createdType,
-          reward: rewardInputRef.current?.value,
+          reward: rewardName,
           expireSelect,
           stampCount: stampCount.value,
         },
@@ -44,7 +41,7 @@ const ModifyCouponPolicy = () => {
       navigate(ROUTER_PATH.customCouponDesign, {
         state: {
           createdType,
-          reward: rewardInputRef.current?.value,
+          reward: rewardName,
           expireSelect,
           stampCount: stampCount.value,
         },
@@ -52,53 +49,65 @@ const ModifyCouponPolicy = () => {
     }
   };
 
+  const moveNextStep = () => {
+    if (step === 3 && !rewardName) {
+      alert('리워드를 작성해주세요.');
+      return;
+    }
+
+    if (step === 4) {
+      navigateNextPage();
+      return;
+    }
+
+    setStep((prev) => (prev += 1));
+  };
+
+  const movePrevStep = () => {
+    setStep((prev) => (prev -= 1));
+  };
+
   return (
     <>
       <Spacing $size={40} />
       <Text variant="pageTitle">쿠폰 제작 및 변경</Text>
       <Spacing $size={40} />
-      <Text variant="subTitle">어떻게 제작하시겠어요?</Text>
-      <Spacing $size={8} />
-      <RadioInputs setValue={setCreatedType} />
-      <Spacing $size={40} />
-      <Text variant="subTitle">목표 스탬프 갯수</Text>
-      <Spacing $size={8} />
-      <Text>리워드를 지급할 스탬프 갯수를 작성해주세요.</Text>
-      <Spacing $size={16} />
-      <SelectBox
-        options={STAMP_COUNT_OPTIONS}
-        checkedOption={stampCount}
-        setCheckedOption={setStampCount}
-      />
-      <Spacing $size={40} />
-      <Input
-        id="create-coupon-reward-input"
-        label="리워드 명"
-        type="text"
-        placeholder="어떤 상품의 리워드를 전달할지 작성해주세요. ex) 아메리카노"
-        required={true}
-        width={400}
-        ref={rewardInputRef}
-      />
-      <Spacing $size={40} />
-      <Text variant="subTitle">쿠폰 유효기간</Text>
-      <Spacing $size={8} />
-      <Text>
-        고객이 가지게 될 쿠폰의 유효기간입니다. 유효기간이 지나면 해당 쿠폰의 스탬프가 모두
-        소멸됩니다.
-      </Text>
-      <Spacing $size={8} />
-      <SelectBox
-        options={EXPIRE_DATE_OPTIONS}
-        checkedOption={expireSelect}
-        setCheckedOption={setExpireSelect}
-      />
-      <Spacing $size={40} />
-      <NextButtonWrapper>
-        <Button variant="secondary" size="medium" onClick={navigateNextPage}>
-          다음으로
-        </Button>
-      </NextButtonWrapper>
+      <StepContainer>
+        <StepWrapper>
+          {(() => {
+            switch (step) {
+              case 1:
+                return <CreatedType setValue={setCreatedType} />;
+              case 2:
+                return (
+                  <MaxStampCount
+                    createdType={createdType}
+                    stampCount={stampCount}
+                    setStampCount={setStampCount}
+                  />
+                );
+              case 3:
+                return <RewardName rewardName={rewardName} setRewardName={setRewardName} />;
+              case 4:
+                return (
+                  <ExpiredPeriod expiredPeriod={expireSelect} setExpiredPeriod={setExpireSelect} />
+                );
+              default:
+                return null;
+            }
+          })()}
+        </StepWrapper>
+        <ButtonContainer $step={step}>
+          {step !== 1 && (
+            <Button variant="secondary" size="medium" onClick={movePrevStep}>
+              이전으로
+            </Button>
+          )}
+          <Button variant="secondary" size="medium" onClick={moveNextStep}>
+            다음으로
+          </Button>
+        </ButtonContainer>
+      </StepContainer>
     </>
   );
 };
--- frontend/src/pages/Admin/ModifyCouponPolicy/style.tsx
+++ frontend/src/pages/Admin/ModifyCouponPolicy/style.tsx
@@ -1,7 +1,19 @@
 import { styled } from 'styled-components';
 
-export const NextButtonWrapper = styled.div`
+export const ButtonContainer = styled.div<{ $step: number }>`
   display: flex;
-  flex-direction: row-reverse;
-  width: 50%;
+  justify-content: ${({ $step }) => ($step === 1 ? 'flex-end' : 'space-between')};
+`;
+
+export const StepContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 40%;
+  height: 60%;
+`;
+
+export const StepWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
