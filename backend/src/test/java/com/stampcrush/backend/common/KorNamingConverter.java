ff5a3af2e23646bbfe3eebe39670882c4cba5e8b
gitchan <eunkeeeee@gmail.com>
2023-08-08 17:26:08 +0900

    feat: @KorNamingConverter 생성

---
+++ backend/src/test/java/com/stampcrush/backend/common/KorNamingConverter.java
@@ -0,0 +1,9 @@
+package com.stampcrush.backend.common;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public @interface KorNamingConverter {
+}
