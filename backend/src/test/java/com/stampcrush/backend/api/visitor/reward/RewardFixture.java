ea8318abbc9d520c5eb2d1bc4d27bd272c6f9bdd
gitchan <eunkeeeee@gmail.com>
2023-08-09 14:49:46 +0900

    feat: RewardFixture에서 시간 필드를 입력하도록 수정

--- backend/src/test/java/com/stampcrush/backend/api/visitor/reward/RewardFixture.java
+++ backend/src/test/java/com/stampcrush/backend/api/visitor/reward/RewardFixture.java
@@ -4,9 +4,13 @@ import com.stampcrush.backend.entity.reward.Reward;
 import com.stampcrush.backend.fixture.CafeFixture;
 import com.stampcrush.backend.fixture.CustomerFixture;
 
+import java.time.LocalDateTime;
+
 public final class RewardFixture {
 
     public static final Reward REWARD_USED_FALSE = new Reward(
+            LocalDateTime.MIN,
+            LocalDateTime.MAX,
             1L,
             "깃짱네 아메리카노",
             false,
@@ -15,6 +19,8 @@ public final class RewardFixture {
     );
 
     public static final Reward REWARD_USED_TRUE = new Reward(
+            LocalDateTime.MIN,
+            LocalDateTime.MAX,
             1L,
             "깃짱네 아메리카노",
             true,
