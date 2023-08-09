67b39e91e574187014ed592cc2157c8c83046154
gitchan <eunkeeeee@gmail.com>
2023-08-09 14:13:19 +0900

    feat: 리워드를 보유 고객과 사용 여부로 필터링해 조회

---
+++ backend/src/test/java/com/stampcrush/backend/repository/reward/RewardRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.stampcrush.backend.repository.reward;
+
+import com.stampcrush.backend.common.KorNamingConverter;
+import com.stampcrush.backend.entity.cafe.Cafe;
+import com.stampcrush.backend.entity.reward.Reward;
+import com.stampcrush.backend.entity.user.Customer;
+import com.stampcrush.backend.entity.user.Owner;
+import com.stampcrush.backend.fixture.CustomerFixture;
+import com.stampcrush.backend.fixture.OwnerFixture;
+import com.stampcrush.backend.repository.cafe.CafeRepository;
+import com.stampcrush.backend.repository.user.CustomerRepository;
+import com.stampcrush.backend.repository.user.OwnerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@KorNamingConverter
+@DataJpaTest
+class RewardRepositoryTest {
+
+    @Autowired
+    private RewardRepository rewardRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CafeRepository cafeRepository;
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @Test
+    void 해당_고객의_사용_가능한_리워드를_조회한다() {
+        Cafe cafe = createCafe(OwnerFixture.GITCHAN);
+        Customer customer = customerRepository.save(CustomerFixture.REGISTER_CUSTOMER_GITCHAN);
+
+        Reward reward = new Reward("깃짱카페의 아메리카노", customer, cafe);
+        Reward savedReward = rewardRepository.save(reward);
+
+        List<Reward> findRewards = rewardRepository.findAllByCustomerAndUsed(customer, false);
+
+        assertThat(findRewards).containsOnly(savedReward);
+    }
+
+    @Test
+    void 해당_고객의_사용_완료한_리워드를_조회한다() {
+        Cafe cafe = createCafe(OwnerFixture.GITCHAN);
+        Customer customer = customerRepository.save(CustomerFixture.REGISTER_CUSTOMER_GITCHAN);
+
+        Reward reward = new Reward("깃짱카페의 아메리카노", customer, cafe);
+        Reward savedReward = rewardRepository.save(reward);
+        savedReward.useReward(customer, cafe);
+
+        List<Reward> findRewards = rewardRepository.findAllByCustomerAndUsed(customer, false);
+
+        assertThat(findRewards).isEmpty();
+    }
+
+    private Cafe createCafe(Owner owner) {
+        Owner savedOwner = ownerRepository.save(owner);
+        return cafeRepository.save(
+                new Cafe(
+                        "깃짱카페",
+                        "서초구",
+                        "어쩌고",
+                        "0101010101",
+                        savedOwner
+                )
+        );
+    }
+}
