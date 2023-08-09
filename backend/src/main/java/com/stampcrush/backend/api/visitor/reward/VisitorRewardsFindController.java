1456d9902a10c7b4ec2b13db53ee0263e7a0e74c
gitchan <eunkeeeee@gmail.com>
2023-08-09 16:08:26 +0900

    feat: 리워드 조회 controller 구현

--- backend/src/main/java/com/stampcrush/backend/api/visitor/reward/VisitorRewardsFindController.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/reward/VisitorRewardsFindController.java
@@ -1,11 +1,31 @@
 package com.stampcrush.backend.api.visitor.reward;
 
+import com.stampcrush.backend.api.visitor.reward.response.VisitorRewardsFindResponse;
+import com.stampcrush.backend.application.visitor.reward.VisitorRewardsFindService;
+import com.stampcrush.backend.application.visitor.reward.dto.VisitorRewardsFindResultDto;
+import com.stampcrush.backend.config.resolver.CustomerAuth;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/rewards")
 public class VisitorRewardsFindController {
+
+    private final VisitorRewardsFindService visitorRewardsFindService;
+
+    @GetMapping
+    public ResponseEntity<VisitorRewardsFindResponse> findRewards(
+            CustomerAuth customer,
+            @RequestParam("used") Boolean used
+    ) {
+        List<VisitorRewardsFindResultDto> result = visitorRewardsFindService.findRewards(customer.getId(), used);
+        return ResponseEntity.ok().body(VisitorRewardsFindResponse.from(result));
+    }
 }
---
+++ backend/src/test/java/com/stampcrush/backend/api/visitor/reward/VisitorRewardsFindControllerTest.java
@@ -0,0 +1,63 @@
+package com.stampcrush.backend.api.visitor.reward;
+
+import com.stampcrush.backend.application.visitor.reward.VisitorRewardsFindService;
+import com.stampcrush.backend.application.visitor.reward.dto.VisitorRewardsFindResultDto;
+import com.stampcrush.backend.config.WebMvcConfig;
+import com.stampcrush.backend.fixture.RewardFixture;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+        value = VisitorRewardsFindController.class,
+        excludeFilters =
+        @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = WebMvcConfig.class
+        )
+)
+class VisitorRewardsFindControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private VisitorRewardsFindService visitorRewardsFindService;
+
+    @Test
+    void 고객모드에서_리워드를_조회한다() throws Exception {
+        when(visitorRewardsFindService.findRewards(null, false))
+                .thenReturn(
+                        List.of(
+                                VisitorRewardsFindResultDto.from(RewardFixture.REWARD_USED_FALSE)
+                        )
+                );
+
+        when(visitorRewardsFindService.findRewards(null, true))
+                .thenReturn(
+                        List.of(
+                                VisitorRewardsFindResultDto.from(RewardFixture.REWARD_USED_TRUE)
+                        )
+                );
+
+        mockMvc.perform(
+                        get("/api/rewards")
+                                .param("used", "false")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("rewards").isNotEmpty());
+    }
+}
