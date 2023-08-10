a155a3e72a673cd0dc56d68927fb42d0fc0d7095
yenawee <yaena0319@naver.com>
2023-08-10 22:39:04 +0900

    refactor: ManagerCouponFindService 에서 방문횟수 구하는 로직 수정

--- backend/src/main/java/com/stampcrush/backend/repository/visithistory/VisitHistoryRepository.java
+++ backend/src/main/java/com/stampcrush/backend/repository/visithistory/VisitHistoryRepository.java
@@ -1,7 +1,13 @@
 package com.stampcrush.backend.repository.visithistory;
 
+import com.stampcrush.backend.entity.cafe.Cafe;
+import com.stampcrush.backend.entity.user.Customer;
 import com.stampcrush.backend.entity.visithistory.VisitHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VisitHistoryRepository extends JpaRepository<VisitHistory, Long> {
+
+    List<VisitHistory> findByCafeAndCustomer(Cafe cafe, Customer customer);
 }
