0c857cdf3e43a191856d190404602c4a31801623
2yunseong <dbsdltjd3701@naver.com>
2023-08-10 17:30:07 +0900

    refactor: 코드 축약

--- frontend/src/pages/RewardHistory/index.tsx
+++ frontend/src/pages/RewardHistory/index.tsx
@@ -21,51 +21,33 @@ export const concatUsedAt = (rewards: Reward[]) => {
   });
 };
 
-// TODO: 타입 구체화
-
-export const transformRewardsByUsedAt = (rewards: Reward[]) => {
+export const transformRewardsToMap = (rewards: Reward[]) => {
   const result = new Map<string, Reward[]>();
 
   const copiedRewards = concatUsedAt(rewards);
 
   copiedRewards.forEach((reward) => {
     if (!reward.usedAt) return;
+    const exists = result.has(reward.usedAt) ? (result.get(reward.usedAt) as Reward[]) : [];
 
-    if (result.has(reward.usedAt)) {
-      const existEntrys = result.get(reward.usedAt) as Reward[];
-      result.set(reward.usedAt, [
-        ...existEntrys,
-        {
-          id: reward.id,
-          cafeName: reward.cafeName,
-          rewardName: reward.rewardName,
-          createdAt: reward.createdAt,
-          usedAt: reward.usedAt,
-        },
-      ]);
-    } else {
-      result.set(reward.usedAt, [
-        {
-          id: reward.id,
-          cafeName: reward.cafeName,
-          rewardName: reward.rewardName,
-          createdAt: reward.createdAt,
-          usedAt: reward.usedAt,
-        },
-      ]);
-    }
+    result.set(reward.usedAt, [
+      ...exists,
+      {
+        ...reward,
+      },
+    ]);
   });
-  return result;
+  return sortMapByKey(result);
 };
 
-function sortMapByKey(map: Map<string, Reward[]>) {
-  const sortedMap = new Map(
+export const sortMapByKey = <V, T extends Map<string, V>>(map: T): T => {
+  const sortedMap = new Map<string, V>(
     [...map.entries()].sort((a, b) => {
       return a[0].localeCompare(b[0]);
     }),
   );
-  return sortedMap;
-}
+  return sortedMap as T;
+};
 
 const RewardHistory = () => {
   const { data: rewardData, status: rewardStatus } = useRewardQuery(true);
@@ -73,15 +55,13 @@ const RewardHistory = () => {
   if (rewardStatus === 'error') return <>에러가 발생했습니다.</>;
   if (rewardStatus === 'loading') return <>로딩 중입니다.</>;
 
-  const transformRewardMap = Array.from(
-    sortMapByKey(transformRewardsByUsedAt(rewardData.rewards)).entries(),
-  );
+  const rewardMap = Array.from(transformRewardsToMap(rewardData.rewards).entries());
 
   return (
     <>
       <SubHeader title="리워드 사용 내역" />
       <div>
-        {transformRewardMap.map(([key, rewards]) => (
+        {rewardMap.map(([key, rewards]) => (
           <div key={key}>
             <RewardDateTitle>{key}</RewardDateTitle>
             {rewards.map((reward) => (
