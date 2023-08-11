1489ca63c31d599d5becce8f893efdad018278d7
gitchan <eunkeeeee@gmail.com>
2023-08-11 17:30:22 +0900

    style: 테스트클래스 이름 변경

---
+++ backend/src/test/java/com/stampcrush/backend/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.stampcrush.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
