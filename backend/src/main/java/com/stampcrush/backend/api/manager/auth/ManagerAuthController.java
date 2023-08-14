84ee2282fb0431c9a09df0c000bd24765b179e54
gitchan <eunkeeeee@gmail.com>
2023-08-14 13:14:32 +0900

    feat: API Post로 변경

--- backend/src/main/java/com/stampcrush/backend/api/manager/auth/ManagerAuthController.java
+++ backend/src/main/java/com/stampcrush/backend/api/manager/auth/ManagerAuthController.java
@@ -3,7 +3,7 @@ package com.stampcrush.backend.api.manager.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class ManagerAuthController {
 
     private final ManagerAuthService managerAuthService;
 
-    @GetMapping("/login/naver")
+    @PostMapping("/login/naver")
     public ResponseEntity<Void> naverLogin() {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(managerAuthService.createLoginRedirectUri()))
