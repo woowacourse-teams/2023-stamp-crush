798189f3dbaff7393f7a79874bd0dec16e668d23
gitchan <eunkeeeee@gmail.com>
2023-08-14 13:03:37 +0900

    feat: 최초 oauth 요청에 대해 redirect uri 응답

---
+++ backend/src/main/java/com/stampcrush/backend/api/manager/auth/ManagerAuthController.java
@@ -0,0 +1,25 @@
+package com.stampcrush.backend.api.manager.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class ManagerAuthController {
+
+    private final ManagerAuthService managerAuthService;
+
+    @GetMapping("/login/naver")
+    public ResponseEntity<Void> naverLogin() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(managerAuthService.createLoginRedirectUri()))
+                .build();
+    }
+}
