36889f8376bdb964f1b958c46ad87eb3162376b2
gitchan <eunkeeeee@gmail.com>
2023-08-14 21:36:41 +0900

    feat: OAuth 관련 코드 작성 (테스트 코드 깨짐)

---
+++ backend/src/main/java/com/stampcrush/backend/auth/KakaoLoginParams.java
@@ -0,0 +1,25 @@
+package com.stampcrush.backend.auth;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
