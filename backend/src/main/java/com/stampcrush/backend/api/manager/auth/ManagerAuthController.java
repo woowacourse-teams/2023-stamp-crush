f1aa937f056673dea280bbb4f3a47a8b2761c42b
yenawee <yaena0319@naver.com>
2023-08-14 14:57:15 +0900

    [BE] feat: Login API Get 으로 변경 (#465)
    * refactor: ManagerAuthService 패키지 이동
    
    * feat: ManagerAuthController GET으로 변경
    
    ---------
    
    Co-authored-by: gitchan <eunkeeeee@gmail.com>

--- backend/src/main/java/com/stampcrush/backend/api/manager/auth/ManagerAuthController.java
+++ backend/src/main/java/com/stampcrush/backend/api/manager/auth/ManagerAuthController.java
@@ -1,9 +1,10 @@
 package com.stampcrush.backend.api.manager.auth;
 
+import com.stampcrush.backend.application.manager.auth.ManagerAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class ManagerAuthController {
 
     private final ManagerAuthService managerAuthService;
 
-    @PostMapping("/login/naver")
+    @GetMapping("/login/naver")
     public ResponseEntity<Void> naverLogin() {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(managerAuthService.createLoginRedirectUri()))
