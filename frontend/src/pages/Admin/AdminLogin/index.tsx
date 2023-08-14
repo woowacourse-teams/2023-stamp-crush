d7bdc9b95b3cdd30a836c485ee6f3cdb02d1f3d6
강영민 <kym6050@hanyang.ac.kr>
2023-08-14 15:01:00 +0900

    [FE] refactor: href로 링크 이동 (#466)
    * feat: oAuth 테스트를 위한 배포
    
    Co-authored-by: 박정규 <srk99279@naver.com>
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>
    
    * refactor: href로 수정
    
    ---------
    
    Co-authored-by: 박정규 <srk99279@naver.com>
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>

--- frontend/src/pages/Admin/AdminLogin/index.tsx
+++ frontend/src/pages/Admin/AdminLogin/index.tsx
@@ -1,3 +1,6 @@
+import { api } from '../../../api';
+import { StampcrushLogo, NaverLoginButton } from '../../../assets';
+import { Container, LogoImg, NaverLoginLink } from './style';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { api, ownerHeader } from '../../../api';
@@ -5,39 +8,17 @@ import { MutateReq } from '../../../types/api';
 import { StampcrushLogo, NaverLoginButton } from '../../../assets';
 import { Container, LoginButton, LogoImg } from './style';
 
-// interface NaverLoginReqBody {
-//   clientId: number;
-// }
-
-// interface NaverLoginRes {
-
-// }
-
 export const postNaverLogin = async () => {
   return await api.post('/admin/login/naver');
 };
 
 const AdminLogin = () => {
-  const navigate = useNavigate();
-  const { mutate: naverLogin } = useMutation({
-    mutationFn: postNaverLogin,
-    onSuccess: (data: any) => {
-      // console.log(data.headers);
-      // navigate(data.headers['location']);
-    },
-  });
-
-  const goNaverLoginPage = () => {
-    naverLogin();
-  };
-
   return (
     <Container>
       <LogoImg src={StampcrushLogo} alt="스탬프크러쉬로고" />
-      <LoginButton onClick={goNaverLoginPage}>
+      <NaverLoginLink href="https://stampcrush.site/api/admin/login/naver">
         <img src={NaverLoginButton} alt="네이버로그인" />
-      </LoginButton>
-      {/* <button>카카오로그인</button> */}
+      </NaverLoginLink>
     </Container>
   );
 };
