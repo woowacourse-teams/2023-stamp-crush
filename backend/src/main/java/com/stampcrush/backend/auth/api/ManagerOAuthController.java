a7f7747e57ebe0acddf2a18daf3ce1e170e210eb
yenawee <yaena0319@naver.com>
2023-08-15 02:15:53 +0900

    feat: jwt 토큰 발급까지 구현(로컬확인)

--- backend/src/main/java/com/stampcrush/backend/auth/api/ManagerOAuthController.java
+++ backend/src/main/java/com/stampcrush/backend/auth/api/ManagerOAuthController.java
@@ -1,29 +1,40 @@
 package com.stampcrush.backend.auth.api;
 
+import com.stampcrush.backend.auth.AuthTokens;
+import com.stampcrush.backend.auth.KakaoLoginParams;
+import com.stampcrush.backend.auth.application.ManagerOAuthLoginService;
 import com.stampcrush.backend.auth.application.ManagerOAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/admin/login")
 @Profile("!test")
 public class ManagerOAuthController {
 
     private final ManagerOAuthService managerOAuthService;
+    private final ManagerOAuthLoginService managerOAuthLoginService;
 
-    @GetMapping("/kakao")
+    @GetMapping("/api/admin/login/kakao")
     public ResponseEntity<Void> loginKakao() {
         String redirectUri = managerOAuthService.findLoginRedirectUri();
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(redirectUri))
                 .build();
     }
+
+    @GetMapping("/admin/login/auth/kakao")
+    public ResponseEntity<AuthTokens> authorizeUser(@RequestParam("code") String code) {
+
+        KakaoLoginParams params = new KakaoLoginParams(code);
+        return ResponseEntity.ok(managerOAuthLoginService.login(params));
+    }
+
 }
