73d215c91a552a15b2066adf501cdf739460df97
gitchan <eunkeeeee@gmail.com>
2023-08-15 20:05:59 +0900

    help: Bean 예외 발생
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/config/resolver/OwnerAuth.java
+++ backend/src/main/java/com/stampcrush/backend/config/resolver/OwnerAuth.java
@@ -3,13 +3,9 @@ package com.stampcrush.backend.config.resolver;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public class OwnerAuth {
 
     private final Long id;
-    private final String name;
-    private final String loginId;
-    private final String encryptedPassword;
-    private final String phoneNumber;
 }
