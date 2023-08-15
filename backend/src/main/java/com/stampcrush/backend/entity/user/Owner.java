8ad6510d4c712b5cb457dd4b5e9cb26c9cd6ef33
gitchan <eunkeeeee@gmail.com>
2023-08-15 15:35:01 +0900

    feat: Owner 객체에 OAuth로 받는 정보 추가 (Transient 필드로 일단 추가함)
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/entity/user/Owner.java
+++ backend/src/main/java/com/stampcrush/backend/entity/user/Owner.java
@@ -1,11 +1,10 @@
 package com.stampcrush.backend.entity.user;
 
+import com.stampcrush.backend.auth.OAuthProvider;
 import com.stampcrush.backend.entity.baseentity.BaseDate;
 import com.stampcrush.backend.exception.OwnerUnAuthorizationException;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,6 +26,12 @@ public class Owner extends BaseDate {
     private String encryptedPassword;
     private String phoneNumber;
 
+    @Transient
+    private String email;
+
+    @Transient
+    private OAuthProvider oAuthProvider;
+
     public Owner(Long id, String nickname, String loginId, String encryptedPassword, String phoneNumber) {
         this.id = id;
         this.nickname = nickname;
@@ -40,6 +45,17 @@ public class Owner extends BaseDate {
         this.loginId = loginId;
         this.encryptedPassword = encryptedPassword;
         this.phoneNumber = phoneNumber;
+    }
+
+    @Builder
+    public Owner(
+            String nickname,
+            String email,
+            OAuthProvider oAuthProvider
+    ) {
+        this.nickname = nickname;
+        this.email = email;
+        this.oAuthProvider = oAuthProvider;
     }
 
     public void checkPassword(String encryptedPassword) {
