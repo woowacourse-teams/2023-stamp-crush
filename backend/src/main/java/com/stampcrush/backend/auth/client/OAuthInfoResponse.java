8739b01f7ae13ba74ea6dd5c2b01bf14efafde33
gitchan <eunkeeeee@gmail.com>
2023-08-15 17:55:26 +0900

    feat: Login에서 구분 식별자 변경 (메서드 중복 추가)
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/auth/client/OAuthInfoResponse.java
+++ backend/src/main/java/com/stampcrush/backend/auth/client/OAuthInfoResponse.java
@@ -4,11 +4,11 @@ import com.stampcrush.backend.auth.OAuthProvider;
 
 public interface OAuthInfoResponse {
 
-    Long getId();
-
     String getNickname();
 
     String getEmail();
 
     OAuthProvider getOAuthProvider();
+
+    Long getOAuthId();
 }
