0c3213b44edbd16f7fa4a563f4c90b50cb16ae0d
yenawee <yaena0319@naver.com>
2023-08-17 01:01:02 +0900

    feat: 고객의 프로필 조회하는 API 구현

--- backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfileFindApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfileFindApiController.java
@@ -1,8 +1,10 @@
 package com.stampcrush.backend.api.visitor.profile;
 
 import com.stampcrush.backend.api.visitor.profile.response.VisitorPhoneNumberFindResponse;
+import com.stampcrush.backend.api.visitor.profile.response.VisitorProfilesFindResponse;
 import com.stampcrush.backend.application.visitor.profile.VisitorProfileFindService;
 import com.stampcrush.backend.application.visitor.profile.dto.VisitorPhoneNumberFindResultDto;
+import com.stampcrush.backend.application.visitor.profile.dto.VisitorProfileFindResultDto;
 import com.stampcrush.backend.config.resolver.CustomerAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,5 +24,13 @@ public class VisitorProfileFindApiController {
         VisitorPhoneNumberFindResultDto dto = visitorProfileFindService.findPhoneNumber(customer.getId());
         return ResponseEntity.ok()
                 .body(VisitorPhoneNumberFindResponse.from(dto));
+    }
+
+    @GetMapping("/profiles")
+    public ResponseEntity<VisitorProfilesFindResponse> findProfiles(CustomerAuth customer) {
+        VisitorProfileFindResultDto dto = visitorProfileFindService.findProfile(customer.getId());
+
+        return ResponseEntity.ok()
+                .body(VisitorProfilesFindResponse.from(dto));
     }
 }
--- backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindService.java
+++ backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindService.java
@@ -1,9 +1,10 @@
 package com.stampcrush.backend.application.visitor.profile;
 
 import com.stampcrush.backend.application.visitor.profile.dto.VisitorPhoneNumberFindResultDto;
-import com.stampcrush.backend.entity.user.Customer;
+import com.stampcrush.backend.application.visitor.profile.dto.VisitorProfileFindResultDto;
+import com.stampcrush.backend.entity.user.RegisterCustomer;
 import com.stampcrush.backend.exception.CustomerNotFoundException;
-import com.stampcrush.backend.repository.user.CustomerRepository;
+import com.stampcrush.backend.repository.user.RegisterCustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,13 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class VisitorProfileFindService {
 
-    private CustomerRepository customerRepository;
+    private RegisterCustomerRepository registerCustomerRepository;
 
     public VisitorPhoneNumberFindResultDto findPhoneNumber(Long customerId) {
-        Customer customer = customerRepository.findById(customerId)
+        // TODO: customerId 왜 int 로 변환해야 하는지 모르겠습니다
+        RegisterCustomer customer = registerCustomerRepository.findById(Math.toIntExact(customerId))
                 .orElseThrow(() -> new CustomerNotFoundException("고객이 존재하지 않습니다"));
 
         String phoneNumber = customer.getPhoneNumber();
         return new VisitorPhoneNumberFindResultDto(phoneNumber);
+    }
+
+    public VisitorProfileFindResultDto findProfile(Long customerId) {
+        RegisterCustomer customer = registerCustomerRepository.findById(Math.toIntExact(customerId))
+                .orElseThrow(() -> new CustomerNotFoundException("고객이 존재하지 않습니다"));
+
+        return new VisitorProfileFindResultDto(customer.getId(), customer.getNickname(), customer.getPhoneNumber(), customer.getEmail());
     }
 }
