8c822104aa4c579ecff9471bb9b2ef1dcd665d38
강영민 <kym6050@hanyang.ac.kr>
2023-08-16 13:48:27 +0900

    [FE] fix: oAuth 테스트 중 발견한 이슈 해결 (#498)
    * fix: 주석 제거
    
    * fix: 테스트 중 발견한 오류 해결
    
    * chore: 주석 제거

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,10 +1,12 @@
-const request = async (path: string, init?: RequestInit) => {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
+import { worker } from '../mocks/browser';
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   worker.start({ onUnhandledRequest: 'bypass' });
-  //   BASE_URL = '';
-  // }
+const request = async (path: string, init?: RequestInit) => {
+  let BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  if (process.env.NODE_ENV === 'development') {
+    worker.start({ onUnhandledRequest: 'bypass' });
+    BASE_URL = '';
+  }
 
   const response = await fetch(`${BASE_URL}${path}`, {
     ...init,
--- frontend/src/pages/Admin/AdminAuth/index.tsx
+++ frontend/src/pages/Admin/AdminAuth/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { getAdminOAuthToken } from '../../../api/get';
+import { ROUTER_PATH } from '../../../constants';
 
 const AdminAuth = () => {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
 
@@ -11,12 +13,14 @@ const AdminAuth = () => {
   }
 
   const getToken = async () => {
-    console.log('dd');
     const response = await getAdminOAuthToken({
       params: { resourceServer: 'kakao', code: code },
     });
 
     localStorage.setItem('admin-login-token', response.accessToken);
+
+    // 카페정보 조회해서 없으면 카페등록페이지, 있으면 고객목록페이지로 가야함.
+    navigate(ROUTER_PATH.customerList);
   };
 
   useEffect(() => {
