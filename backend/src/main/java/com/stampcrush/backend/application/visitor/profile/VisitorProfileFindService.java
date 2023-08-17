e1d3c541964470fe3639e9647bb7297b7a1d8c64
gitchan <eunkeeeee@gmail.com>
2023-08-17 10:31:55 +0900

    fix: 서비스 코드에 레포지토리 DI 수정

--- backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindService.java
+++ backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindService.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class VisitorProfileFindService {
 
-    private RegisterCustomerRepository registerCustomerRepository;
+    private final RegisterCustomerRepository registerCustomerRepository;
 
     public VisitorProfileFindResultDto findProfile(Long customerId) {
         RegisterCustomer customer = registerCustomerRepository.findById(customerId)
