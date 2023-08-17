e0fb44af0e78777255bc5178f0fc8f32416e4578
yenawee <yaena0319@naver.com>
2023-08-17 10:14:53 +0900

    feat: ServiceTest 작성중

--- backend/src/test/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindServiceTest.java
+++ backend/src/test/java/com/stampcrush/backend/application/visitor/profile/VisitorProfileFindServiceTest.java
@@ -1,18 +1,10 @@
 package com.stampcrush.backend.application.visitor.profile;
 
 import com.stampcrush.backend.application.ServiceSliceTest;
-import com.stampcrush.backend.application.visitor.profile.dto.VisitorPhoneNumberFindResultDto;
 import com.stampcrush.backend.repository.user.RegisterCustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import java.util.Optional;
-
-import static com.stampcrush.backend.fixture.CustomerFixture.REGISTER_CUSTOMER_JENA;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 
 @ServiceSliceTest
 class VisitorProfileFindServiceTest {
@@ -24,16 +16,8 @@ class VisitorProfileFindServiceTest {
     private RegisterCustomerRepository customerRepository;
 
     @Test
-    void 고객의_전화번호가_등록되어_있으면_전화번호_반환() {
+    void 고객의_프로필_정보를_조회한다() {
         // given
-        when(customerRepository.findById(anyInt()))
-                .thenReturn(Optional.of(REGISTER_CUSTOMER_JENA));
 
-        // when
-        VisitorPhoneNumberFindResultDto expected = new VisitorPhoneNumberFindResultDto(REGISTER_CUSTOMER_JENA.getPhoneNumber());
-        VisitorPhoneNumberFindResultDto resultDto = visitorProfileFindService.findPhoneNumber(1L);
-
-        // then
-        assertThat(resultDto).usingRecursiveComparison().isEqualTo(expected);
     }
 }
