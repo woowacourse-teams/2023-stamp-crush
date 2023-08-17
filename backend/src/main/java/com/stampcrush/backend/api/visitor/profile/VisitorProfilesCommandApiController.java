1bfff9cfa50ec0c9e432337e2b11ff5b40aca1c4
gitchan <eunkeeeee@gmail.com>
2023-08-17 17:06:59 +0900

    fix: ResponseBody 어노테이션 추가

--- backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfilesCommandApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfilesCommandApiController.java
@@ -3,9 +3,11 @@ package com.stampcrush.backend.api.visitor.profile;
 import com.stampcrush.backend.api.visitor.profile.request.VisitorProfilesPhoneNumberUpdateRequest;
 import com.stampcrush.backend.application.visitor.profile.VisitorProfilesCommandService;
 import com.stampcrush.backend.config.resolver.CustomerAuth;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,7 @@ public class VisitorProfilesCommandApiController {
     @PostMapping("/phone-number")
     public ResponseEntity<Void> savePhoneNumber(
             CustomerAuth customer,
-            VisitorProfilesPhoneNumberUpdateRequest request
+            @Valid @RequestBody VisitorProfilesPhoneNumberUpdateRequest request
     ) {
         System.out.println("request.getPhoneNumber() = " + request.getPhoneNumber());
         visitorProfilesCommandService.registerPhoneNumber(customer.getId(), request.getPhoneNumber());
--- backend/src/main/java/com/stampcrush/backend/api/visitor/profile/request/VisitorProfilesPhoneNumberUpdateRequest.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/profile/request/VisitorProfilesPhoneNumberUpdateRequest.java
@@ -1,17 +1,17 @@
 package com.stampcrush.backend.api.visitor.profile.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class VisitorProfilesPhoneNumberUpdateRequest {
 
+    @NotNull
+    @NotBlank
     private String phoneNumber;
-
-    public VisitorProfilesPhoneNumberUpdateRequest(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public VisitorProfilesPhoneNumberUpdateRequest() {
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 }
