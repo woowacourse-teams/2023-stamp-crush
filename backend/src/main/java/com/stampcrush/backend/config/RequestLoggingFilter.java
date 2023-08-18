99eea8ecc1d113a2d87585a99cf75f064d1d8db1
youngh0 <cyh6099@gmail.com>
2023-08-18 10:40:40 +0900

    feat: request 정보 logging (#588)

---
+++ backend/src/main/java/com/stampcrush/backend/config/RequestLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.stampcrush.backend.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info(request.getRequestURI());
+        if (request.getHeader("authorization") != null) {
+            log.info("auth info: " + request.getHeader("authorization"));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
