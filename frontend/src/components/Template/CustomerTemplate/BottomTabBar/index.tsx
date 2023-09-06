ae7e8295556cc4e7f0b890d178e6a65fbde2306b
regularPark <srk99279@naver.com>
2023-09-06 17:28:32 +0900

    refactor: 중복되는 코드 리팩터링 및 현재 선택된 페이지 알 수 있도록 스타일 수정

--- frontend/src/components/Template/CustomerTemplate/BottomTabBar/index.tsx
+++ frontend/src/components/Template/CustomerTemplate/BottomTabBar/index.tsx
@@ -1,32 +1,49 @@
 import { Link, useLocation } from 'react-router-dom';
-import { TabBarContainer } from './style';
+import { TabBarContainer, TapBarItem } from './style';
 import { AiOutlineHome, AiOutlineGift, AiOutlineUser } from 'react-icons/ai';
 import { ROUTER_PATH } from '../../../../constants';
+import { RouterPath } from '../../../../types';
+const tabs = [
+  { path: [ROUTER_PATH.couponList], icon: <AiOutlineHome size={28} />, label: '홈' },
+  { path: [ROUTER_PATH.rewardList], icon: <AiOutlineGift size={28} />, label: '리워드' },
+  {
+    path: [ROUTER_PATH.myPage, ROUTER_PATH.rewardHistory, ROUTER_PATH.stampHistory],
+    icon: <AiOutlineUser size={28} />,
+    label: '마이페이지',
+  },
+];
 
 const BottomTabBar = () => {
   const location = useLocation();
 
-  if (
-    location.pathname === ROUTER_PATH.login ||
-    location.pathname === ROUTER_PATH.auth ||
-    location.pathname === ROUTER_PATH.signup ||
-    location.pathname === ROUTER_PATH.inputPhoneNumber
-  )
+  const shouldDisplayTabBar = ![
+    ROUTER_PATH.login,
+    ROUTER_PATH.auth,
+    ROUTER_PATH.signup,
+    ROUTER_PATH.inputPhoneNumber,
+  ].includes(location.pathname as RouterPath);
+
+  if (!shouldDisplayTabBar) {
     return null;
+  }
+
+  console.log(
+    tabs.map((tab, index) => {
+      console.log(tab.path);
+      console.log(tab.path.includes(location.pathname as RouterPath));
+    }),
+  );
 
   return (
     <TabBarContainer>
-      <Link to="/">
-        <AiOutlineHome size={28} />홈
-      </Link>
-      <Link to="/reward-list">
-        <AiOutlineGift size={28} />
-        리워드
-      </Link>
-      <Link to="/my-page">
-        <AiOutlineUser size={28} />
-        마이페이지
-      </Link>
+      {tabs.map((tab, index) => (
+        <TapBarItem key={index} $isSelected={tab.path.includes(location.pathname as RouterPath)}>
+          <Link to={Array.isArray(tab.path) ? tab.path[0] : tab.path}>
+            {tab.icon}
+            {tab.label}
+          </Link>
+        </TapBarItem>
+      ))}
     </TabBarContainer>
   );
 };
--- frontend/src/components/Template/CustomerTemplate/BottomTabBar/style.tsx
+++ frontend/src/components/Template/CustomerTemplate/BottomTabBar/style.tsx
@@ -12,16 +12,20 @@ export const TabBarContainer = styled.div`
   border-radius: 8px 8px 0 0;
   box-shadow: 0px -4px 8px 0 rgba(0, 0, 0, 0.1);
   background: white;
+`;
 
+export const TapBarItem = styled.div<{ $isSelected: boolean }>`
   & > a {
     display: flex;
     width: 70px;
     flex-direction: column;
     align-items: center;
-    color: black;
+    color: ${({ $isSelected }) => ($isSelected ? 'black' : '#999')};
 
     font-size: 14px;
     text-decoration: none;
+    transform: ${({ $isSelected }) => ($isSelected ? 'Scale(1.05)' : null)};
+    transition: all 0.2s ease-in-out;
     gap: 4px;
   }
 `;
