14baa4135618c9fb132c2dd575e62ebec7f769b9
kangyeongmin <kym6050@hanyang.ac.kr>
2023-09-06 17:58:30 +0900

    feat: 첫 적립일 때, 전화번호를 확인하는 로직 구현

--- frontend/src/pages/Admin/EnterPhoneNumber/Dialpad/index.tsx
+++ frontend/src/pages/Admin/EnterPhoneNumber/Dialpad/index.tsx
@@ -1,4 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { getCustomer } from '../../../../api/get';
+import Alert from '../../../../components/Alert';
 import useDialPad from '../../../../hooks/useDialPad';
+import useModal from '../../../../hooks/useModal';
+import { CustomerPhoneNumberRes } from '../../../../types/api';
 import { BaseInput, Container, KeyContainer, Pad } from './style';
 
 const DIAL_KEYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '←', '0', '입력'] as const;
@@ -6,11 +12,48 @@ const DIAL_KEYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '←', '0', '입
 export type DialKeyType = (typeof DIAL_KEYS)[number];
 
 const Dialpad = () => {
+  const { isOpen, openModal, closeModal } = useModal();
+  const [isDone, setIsDone] = useState(false);
   const { phoneNumber, phoneNumberRef, handlePhoneNumber, handleBackspace, pressPad } =
     useDialPad();
 
+  const { status: customerStatus } = useQuery<CustomerPhoneNumberRes>(['customer', phoneNumber], {
+    queryFn: () => getCustomer({ params: { phoneNumber } }),
+    onSuccess: (data) => {
+      if (data.customer.length === 0) {
+        openModal();
+      }
+    },
+    enabled: isDone,
+  });
+
+  if (customerStatus === 'error') return <div>Error</div>;
+
+  const sendPhoneNumber = (dialKey: DialKeyType) => () => {
+    if (dialKey === '입력') {
+      setIsDone(true);
+      return;
+    }
+
+    pressPad(dialKey)();
+  };
+
+  const retryPhoneNumber = () => {
+    closeModal();
+    setIsDone(false);
+  };
+
   return (
     <Container>
+      {isOpen && (
+        <Alert
+          text={phoneNumber + '님, 첫 적립이 맞으신가요?'}
+          rightOption={'네'}
+          leftOption={'다시 입력'}
+          onClickRight={pressPad('입력')}
+          onClickLeft={retryPhoneNumber}
+        />
+      )}
       <BaseInput
         id="phoneNumber"
         value={phoneNumber}
@@ -26,7 +69,7 @@ const Dialpad = () => {
       />
       <KeyContainer>
         {DIAL_KEYS.map((dialKey) => (
-          <Pad key={dialKey} onClick={pressPad(dialKey)}>
+          <Pad key={dialKey} onClick={sendPhoneNumber(dialKey)}>
             {dialKey}
           </Pad>
         ))}
