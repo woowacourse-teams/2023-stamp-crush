6d4b51c859b5bfe3a52a9b59f52963f8e283d43e
2yunseong <dbsdltjd3701@naver.com>
2023-09-06 14:54:25 +0900

    feat: 고객 로그인 페이지 디자인 변경

--- frontend/src/pages/Login/style.tsx
+++ frontend/src/pages/Login/style.tsx
@@ -1,15 +1,55 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
+  font-family: sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
-  gap: 100px;
 
   & > img {
     width: 200px;
   }
+`;
+
+export const ServiceIntro = styled.span`
+  margin-top: 42px;
+  font-weight: 400;
+  font-size: 20px;
+`;
+
+export const ServiceIntroSub = styled.span`
+  margin-top: 8px;
+  font-weight: 300;
+  font-size: 16px;
+`;
+
+export const LogoImg = styled.img`
+  height: 100px;
+`;
+
+export const LoginLink = styled.a`
+  margin-top: 42px;
+`;
+
+export const KakaoLoginImg = styled.img`
+  width: 240px;
+  height: 50px;
+`;
+
+export const RedirectContainer = styled.div`
+  margin-top: 18px;
+  font-size: 12px;
+`;
+
+export const RedirectLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.gray400};
+`;
+
+export const Title = styled.h2`
+  font-size: 36px;
+  font-weight: 700;
 `;
