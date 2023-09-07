252653ca68a4b4351d64606adfcf9fc09a57da0f
박정규 <srk99279@naver.com>
2023-09-07 16:37:23 +0900

    [FE] feat: 고객모드 메인페이지를 개편한다. (#625)
    * fix: 환경변수 미적용 부분 수정
    
    * feat: `BottomTapBar` 생성 및 적용
    
    * refactor: 디렉터리 변경 및 애니메이션 제거
    
    * style: `Main` 컴포넌트 제거
    
    * refactor: 쿠폰 클릭 시 상세정보 보기 기능 추가 및 상세정보 버튼 제거
    
    * refactor: `BottomTapBar` 아이콘 변경
    
    * feat: 모바일 UX 개선 위한 터치 이벤트 적용
    
    * refactor: 콘솔 제거 및 early return
    
    * refactor: 모바일 UX 개선 위한 `FlippedCoupon` 애니메이션 시간 수정
    
    * refactor: 중복되는 코드 리팩터링 및 현재 선택된 페이지 알 수 있도록 스타일 수정
    
    * style: 콘솔 로그 제거
    
    * refactor: 마이페이지의 내 리워드 제거
    
    * refactor: 상수 네이밍 변경
    
    * refactor: 변수 네이밍 변경
    
    * refactor: 불필요한 로직 제거
    
    * refactor: 변수 네이밍 변경
    
    * design: position:absolute -> fixed로 변경
    
    * refactor: `TabBar`가 보이는 조건 변경
    
    * refactor: 모바일 UX 개선 작업
    
    메인 페이지에서 `touch-action:none`을 사용하여 쿠폰을 스와이프할 때 다른 화면이 출렁거리는 것을 방지하였음

--- frontend/src/style/keyframes.ts
+++ frontend/src/style/keyframes.ts
@@ -11,17 +11,6 @@ export const swap = keyframes`
   }
 `;
 
-export const detail = keyframes`
-  0% {
-    transform: translateY(15px) scale(1.05);
-    display: flex;
-  }
-  
-  100%{
-    transform: translateY(-219%) scale(0.86);
-  }
-`;
-
 export const popup = keyframes`
   0% {
     transform: translateY(100%);
