9b60875e0299c5b50e29d92a4f44bfe9c7d31ced
regularPark <srk99279@naver.com>
2023-09-07 16:17:54 +0900

    refactor: 모바일 UX 개선 작업
    메인 페이지에서 `touch-action:none`을 사용하여 쿠폰을 스와이프할 때 다른 화면이 출렁거리는 것을 방지하였음

--- frontend/src/components/Template/CustomerTemplate/index.tsx
+++ frontend/src/components/Template/CustomerTemplate/index.tsx
@@ -1,10 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { BaseCustomerTemplate, ContentContainer } from './style';
 import BottomTabBar from './BottomTabBar';
+import { useLocation } from 'react-router-dom';
+import { ROUTER_PATH } from '../../../constants';
 
 const CustomerTemplate = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+
   return (
-    <BaseCustomerTemplate>
+    <BaseCustomerTemplate $isHome={location.pathname === ROUTER_PATH.couponList}>
       <ContentContainer>
         {children}
         <BottomTabBar />
