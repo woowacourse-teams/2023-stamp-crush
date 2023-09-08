ba2925479f3642c17d3d690ad9cddab7c2ee8736
박정규 <srk99279@naver.com>
2023-09-08 16:41:08 +0900

    [FE] refactor: 로그인 상태를 판별하는 PrivateProvider를 구현한다.  (#637)
    * feat: `Provider`를 이용한 Admin로그인 유무 검사
    
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>
    
    * feat: Admin, Customer 로그인을 관장하는 Provider 구현
    
    ---------
    
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>

--- frontend/src/pages/Admin/EnterPhoneNumber/index.tsx
+++ frontend/src/pages/Admin/EnterPhoneNumber/index.tsx
@@ -4,6 +4,7 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import { ROUTER_PATH } from '../../../constants';
 import { useRedirectRegisterPage } from '../../../hooks/useRedirectRegisterPage';
+import PrivateProvider from '../../../provider/PrivateProvider';
 
 const EnterPhoneNumber = () => {
   useRedirectRegisterPage();
@@ -14,21 +15,23 @@ const EnterPhoneNumber = () => {
   };
 
   return (
-    <PageContainer>
-      <Title>
-        <IconWrapper onClick={navigateBack}>
-          <IoIosArrowBack size="40" />
-        </IconWrapper>
-        전화번호 입력
-      </Title>
-      <Container>
-        <PrivacyBox>
-          <h1>개인정보 제공동의</h1>
-          <p>전화번호를 입력하시면 개인정보 제공에 동의하시는 것으로 간주됩니다.</p>
-        </PrivacyBox>
-        <Dialpad />
-      </Container>
-    </PageContainer>
+    <PrivateProvider consumer={'admin'}>
+      <PageContainer>
+        <Title>
+          <IconWrapper onClick={navigateBack}>
+            <IoIosArrowBack size="40" />
+          </IconWrapper>
+          전화번호 입력
+        </Title>
+        <Container>
+          <PrivacyBox>
+            <h1>개인정보 제공동의</h1>
+            <p>전화번호를 입력하시면 개인정보 제공에 동의하시는 것으로 간주됩니다.</p>
+          </PrivacyBox>
+          <Dialpad />
+        </Container>
+      </PageContainer>
+    </PrivateProvider>
   );
 };
 
--- frontend/src/pages/Customer/Login/index.tsx
+++ frontend/src/pages/Customer/Login/index.tsx
@@ -9,24 +9,27 @@ import {
   ServiceIntro,
   ServiceIntroSub,
   RedirectContainer,
+  Template,
 } from './style';
 
 const Login = () => {
   const KAKAO_LOGIN_PAGE_URL = `${BASE_URL}/login/kakao`;
 
   return (
-    <Container>
-      <LogoImg src={LoginLogo} />
-      <ServiceIntro>흩어져있는 종이 쿠폰을 한번에!</ServiceIntro>
-      <ServiceIntroSub>카페쿠폰 관리 서비스</ServiceIntroSub>
-      <LoginLink href={KAKAO_LOGIN_PAGE_URL}>
-        <KakaoLoginImg src={CustomerKakaoLoginButton} alt="카카오 로그인" />
-      </LoginLink>
-      <RedirectContainer>
-        <span>사장님이신가요? </span>
-        <RedirectLink to={ROUTER_PATH.adminLogin}>사장님 로그인</RedirectLink>
-      </RedirectContainer>
-    </Container>
+    <Template>
+      <Container>
+        <LogoImg src={LoginLogo} />
+        <ServiceIntro>흩어져있는 종이 쿠폰을 한번에!</ServiceIntro>
+        <ServiceIntroSub>카페쿠폰 관리 서비스</ServiceIntroSub>
+        <LoginLink href={KAKAO_LOGIN_PAGE_URL}>
+          <KakaoLoginImg src={CustomerKakaoLoginButton} alt="카카오 로그인" />
+        </LoginLink>
+        <RedirectContainer>
+          <span>사장님이신가요? </span>
+          <RedirectLink to={ROUTER_PATH.adminLogin}>사장님 로그인</RedirectLink>
+        </RedirectContainer>
+      </Container>
+    </Template>
   );
 };
 
