19b4b2a56bfe8d7cd063d222cf7d0b42b11e8d74
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-09-08 15:51:31 +0900

    [FE] 로그인 페이지를 개선한다. (#624)
    * feat: 고객 로그인 페이지 디자인 변경
    
    * refactor: 네이밍 변경
    
    * chore: 필요한 asset 추가
    
    * feat: 사장님 로그인 페이지 개선
    
    * refactor: padding 값 수정
    
    * fix: 가운데 정렬로 변경
    
    * refactor: 카카오 로그인 아이콘 개선
    
    * fix: 리뷰 반영
    
    * refactor: 로그인 홍보 문구 수정

--- frontend/src/pages/Customer/Login/index.tsx
+++ frontend/src/pages/Customer/Login/index.tsx
@@ -1,17 +1,31 @@
-import { LoginLogo } from '../../../assets';
-import { CustomerKakaoLoginButton } from '../../../assets';
-import { BASE_URL } from '../../../constants';
-import { Container } from './style';
+import { LoginLogo, CustomerKakaoLoginButton } from '../../../assets';
+import { BASE_URL, ROUTER_PATH } from '../../../constants';
+import {
+  Container,
+  KakaoLoginImg,
+  LoginLink,
+  LogoImg,
+  RedirectLink,
+  ServiceIntro,
+  ServiceIntroSub,
+  RedirectContainer,
+} from './style';
 
 const Login = () => {
   const KAKAO_LOGIN_PAGE_URL = `${BASE_URL}/login/kakao`;
 
   return (
     <Container>
-      <img src={LoginLogo} />
-      <a href={KAKAO_LOGIN_PAGE_URL}>
-        <img src={CustomerKakaoLoginButton} />
-      </a>
+      <LogoImg src={LoginLogo} />
+      <ServiceIntro>흩어져있는 종이 쿠폰을 한번에!</ServiceIntro>
+      <ServiceIntroSub>카페쿠폰 관리 서비스</ServiceIntroSub>
+      <LoginLink href={KAKAO_LOGIN_PAGE_URL}>
+        <KakaoLoginImg src={CustomerKakaoLoginButton} alt="카카오 로그인" />
+      </LoginLink>
+      <RedirectContainer>
+        <span>사장님이신가요? </span>
+        <RedirectLink to={ROUTER_PATH.adminLogin}>사장님 로그인</RedirectLink>
+      </RedirectContainer>
     </Container>
   );
 };
