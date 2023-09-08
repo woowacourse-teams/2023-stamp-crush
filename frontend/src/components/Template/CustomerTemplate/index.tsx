87da6498b2407099576186c98a612df0a51b020f
regularPark <srk99279@naver.com>
2023-09-08 17:31:38 +0900

    refactor: `TabBar`의 렌더링 계층을 변경

--- frontend/src/components/Template/CustomerTemplate/index.tsx
+++ frontend/src/components/Template/CustomerTemplate/index.tsx
@@ -3,15 +3,22 @@ import { BaseCustomerTemplate, ContentContainer } from './style';
 import BottomTabBar from './BottomTabBar';
 import { useLocation } from 'react-router-dom';
 import { ROUTER_PATH } from '../../../constants';
+import { RouterPath } from '../../../types';
 
 const CustomerTemplate = ({ children }: PropsWithChildren) => {
   const location = useLocation();
+
+  const isTabBarVisible = [
+    ROUTER_PATH.couponList,
+    ROUTER_PATH.rewardList,
+    ROUTER_PATH.myPage,
+  ].includes(location.pathname as RouterPath);
 
   return (
     <BaseCustomerTemplate $isHome={location.pathname === ROUTER_PATH.couponList}>
       <ContentContainer>
         {children}
-        <BottomTabBar />
+        {isTabBarVisible && <BottomTabBar />}
       </ContentContainer>
     </BaseCustomerTemplate>
   );
