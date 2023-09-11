57fb58fc8df035578b89359c14e5175e9d7aa01d
gitchan <eunkeeeee@gmail.com>
2023-09-11 16:52:19 +0900

    refactor: 생성자 어노테이션으로 대체

--- backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfilesLinkDataRequest.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorProfilesLinkDataRequest.java
@@ -1,18 +1,15 @@
 package com.stampcrush.backend.api.visitor.profile;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisitorProfilesLinkDataRequest {
 
     private Long id;
-
-    public VisitorProfilesLinkDataRequest(Long id) {
-        this.id = id;
-    }
-
-    public VisitorProfilesLinkDataRequest() {
-    }
 
     public VisitorProfilesLinkDataDto toDto() {
         return new VisitorProfilesLinkDataDto(id);
