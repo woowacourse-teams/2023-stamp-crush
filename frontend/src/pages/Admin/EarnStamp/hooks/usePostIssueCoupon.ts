c59f8205506aa0aa4627b378c652f50c92ef102d
regularPark <srk99279@naver.com>
2023-09-11 21:18:42 +0900

    feat: `usePostIssueCoupon` 생성

---
+++ frontend/src/pages/Admin/EarnStamp/hooks/usePostIssueCoupon.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { IssueCouponRes } from '../../../../types/api';
+import { postIssueCoupon } from '../../../../api/post';
+
+const usePostIssueCoupon = (cafeId: number, customer: any) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<IssueCouponRes>({
+    mutationFn: async () => {
+      if (!customer) throw new Error('고객 정보를 불러오지 못했습니다.');
+      return await postIssueCoupon({
+        params: { customerId: customer.id },
+        body: {
+          cafeId,
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['coupon'] });
+    },
+    onError: () => {
+      throw new Error('스탬프 적립에 실패했습니다.');
+    },
+  });
+};
+
+export default usePostIssueCoupon;
