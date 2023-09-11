ed4e28c665e13dc661963f1cb95eafd153d6d0b7
Youngho <cyh6099@gmail.com>
2023-09-12 00:00:06 +0900

    feat: mdc 를 이용한 스레드 별 로깅

--- backend/src/main/java/com/stampcrush/backend/api/GlobalExceptionHandler.java
+++ backend/src/main/java/com/stampcrush/backend/api/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.stampcrush.backend.api;
 
-import com.stampcrush.backend.exception.*;
+import com.stampcrush.backend.exception.BadRequestException;
+import com.stampcrush.backend.exception.ForbiddenException;
+import com.stampcrush.backend.exception.NotFoundException;
+import com.stampcrush.backend.exception.StampCrushException;
+import com.stampcrush.backend.exception.UnAuthorizationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -21,6 +30,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
+        log.warn("NotFoundException | " + exception.getMessage());
         return ResponseEntity
                 .status(NOT_FOUND.value())
                 .body(new ExceptionResponse(exception.getMessage()));
@@ -28,6 +38,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException exception) {
+        log.warn("BadRequestException | " + exception.getMessage());
         return ResponseEntity
                 .status(BAD_REQUEST.value())
                 .body(new ExceptionResponse(exception.getMessage()));
@@ -35,6 +46,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ExceptionResponse> handleForbiddenException(ForbiddenException exception) {
+        log.warn("ForbiddenException | " + exception.getMessage());
         return ResponseEntity
                 .status(FORBIDDEN.value())
                 .body(new ExceptionResponse(exception.getMessage()));
@@ -42,6 +54,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UnAuthorizationException.class)
     public ResponseEntity<ExceptionResponse> handleUnAuthorizationException(UnAuthorizationException exception) {
+        log.warn("UnAuthorizationException | " + exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED.value())
                 .body(new ExceptionResponse(exception.getMessage()));
--- backend/src/main/java/com/stampcrush/backend/config/RequestLoggingFilter.java
+++ backend/src/main/java/com/stampcrush/backend/config/RequestLoggingFilter.java
@@ -5,21 +5,27 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class RequestLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.info("method: " + request.getMethod() + ", url: " + request.getRequestURI());
-        if (request.getHeader("authorization") != null) {
-            log.info("auth info: " + request.getHeader("authorization"));
-        }
+        MDC.put("requestId", UUID.randomUUID().toString());
+        MDC.put("requestMethod", request.getMethod());
+        MDC.put("requestUrl", request.getRequestURI());
 
+        String authorization = request.getHeader("authorization");
+        MDC.put("authorization", authorization);
+        if (authorization == null) {
+            MDC.put("authorization", "null");
+        }
         filterChain.doFilter(request, response);
     }
 }
