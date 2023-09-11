427637b14d06cf5347d4cb478e38279fe971c507
JunDonghyuk <ehdgur4814@naver.com>
2023-09-12 02:04:46 +0900

    refactor: Cafe-Owner 검증 추가

--- backend/src/main/java/com/stampcrush/backend/application/manager/cafe/ManagerCafeCouponSettingFindService.java
+++ backend/src/main/java/com/stampcrush/backend/application/manager/cafe/ManagerCafeCouponSettingFindService.java
@@ -6,12 +6,15 @@ import com.stampcrush.backend.entity.cafe.Cafe;
 import com.stampcrush.backend.entity.cafe.CafeCouponDesign;
 import com.stampcrush.backend.entity.coupon.Coupon;
 import com.stampcrush.backend.entity.coupon.CouponDesign;
+import com.stampcrush.backend.entity.user.Owner;
 import com.stampcrush.backend.exception.CafeCouponSettingNotFoundException;
 import com.stampcrush.backend.exception.CafeNotFoundException;
 import com.stampcrush.backend.exception.CouponNotFoundException;
+import com.stampcrush.backend.exception.OwnerNotFoundException;
 import com.stampcrush.backend.repository.cafe.CafeCouponDesignRepository;
 import com.stampcrush.backend.repository.cafe.CafeRepository;
 import com.stampcrush.backend.repository.coupon.CouponRepository;
+import com.stampcrush.backend.repository.user.OwnerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,10 +29,14 @@ public class ManagerCafeCouponSettingFindService {
     private final CafeRepository cafeRepository;
     private final CouponRepository couponRepository;
     private final CafeCouponDesignRepository cafeCouponDesignRepository;
+    private final OwnerRepository ownerRepository;
 
-    public CafeCouponSettingFindResultDto findCafeCouponSetting(Long cafeId) {
+    public CafeCouponSettingFindResultDto findCafeCouponSetting(Long ownerId, Long cafeId) {
         Cafe cafe = cafeRepository.findById(cafeId)
                 .orElseThrow(() -> new CafeNotFoundException("카페를 찾지 못했습니다."));
+        Owner owner = ownerRepository.findById(ownerId)
+                .orElseThrow(() -> new OwnerNotFoundException("사장을 찾지 못했습니다."));
+        cafe.validateOwnership(owner);
         CafeCouponDesign cafeCouponDesign = cafeCouponDesignRepository.findByCafe(cafe)
                 .orElseThrow(() -> new CafeCouponSettingNotFoundException("카페 디자인을 찾을 수 없습니다."));
         return new CafeCouponSettingFindResultDto(
@@ -43,9 +50,12 @@ public class ManagerCafeCouponSettingFindService {
                                 stamp.getYCoordinate())).toList());
     }
 
-    public CafeCouponSettingFindResultDto findCouponSetting(Long cafeId, Long couponId) {
+    public CafeCouponSettingFindResultDto findCouponSetting(Long ownerId, Long cafeId, Long couponId) {
         Cafe cafe = cafeRepository.findById(cafeId)
                 .orElseThrow(() -> new CafeNotFoundException("카페를 찾지 못했습니다."));
+        Owner owner = ownerRepository.findById(ownerId)
+                .orElseThrow(() -> new OwnerNotFoundException("사장을 찾지 못했습니다."));
+        cafe.validateOwnership(owner);
         Coupon coupon = couponRepository.findById(couponId)
                 .orElseThrow(() -> new CouponNotFoundException("couponId: " + couponId + " 쿠폰을 찾지 못했습니다."));
         CouponDesign couponDesign = coupon.getCouponDesign();
