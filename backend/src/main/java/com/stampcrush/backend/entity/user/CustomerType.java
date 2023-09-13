aab2034d3e29720da32bee0614cf9b6c193fc1ca
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-09-13 15:52:33 +0900

    [BE] fix: CustomerType에서 이름을 거꾸로 지정한 문제 해결 (#668)
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/entity/user/CustomerType.java
+++ backend/src/main/java/com/stampcrush/backend/entity/user/CustomerType.java
@@ -2,16 +2,6 @@ package com.stampcrush.backend.entity.user;
 
 public enum CustomerType {
 
-    TEMPORARY("register"),
-    REGISTERED("temporary");
-
-    private final String customerType;
-
-    CustomerType(String customerType) {
-        this.customerType = customerType;
-    }
-
-    public String getCustomerType() {
-        return customerType;
-    }
+    TEMPORARY,
+    REGISTER;
 }
