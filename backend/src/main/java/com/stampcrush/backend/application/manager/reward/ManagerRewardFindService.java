dede2244c1bcbce2d9942905f097022106c6ce7b
yenawee <yaena0319@naver.com>
2023-09-14 17:07:41 +0900

    feat: 방문횟수 없을 시 카페 고객이 아니라 판단해 에러 발생

--- backend/src/main/java/com/stampcrush/backend/application/manager/reward/ManagerRewardFindService.java
+++ backend/src/main/java/com/stampcrush/backend/application/manager/reward/ManagerRewardFindService.java
@@ -5,11 +5,14 @@ import com.stampcrush.backend.application.manager.reward.dto.RewardFindResultDto
 import com.stampcrush.backend.entity.cafe.Cafe;
 import com.stampcrush.backend.entity.reward.Reward;
 import com.stampcrush.backend.entity.user.Owner;
+import com.stampcrush.backend.entity.visithistory.VisitHistory;
 import com.stampcrush.backend.exception.CafeNotFoundException;
 import com.stampcrush.backend.exception.OwnerNotFoundException;
+import com.stampcrush.backend.exception.OwnerUnAuthorizationException;
 import com.stampcrush.backend.repository.cafe.CafeRepository;
 import com.stampcrush.backend.repository.reward.RewardRepository;
 import com.stampcrush.backend.repository.user.OwnerRepository;
+import com.stampcrush.backend.repository.visithistory.VisitHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +27,15 @@ public class ManagerRewardFindService {
     private final RewardRepository rewardRepository;
     private final CafeRepository cafeRepository;
     private final OwnerRepository ownerRepository;
+    private final VisitHistoryRepository visitHistoryRepository;
 
     public List<RewardFindResultDto> findRewards(Long ownerId, RewardFindDto rewardFindDto) {
+        List<VisitHistory> visitHistories = visitHistoryRepository.findByCafeIdAndCustomerId(rewardFindDto.getCafeId(), rewardFindDto.getCustomerId());
+
+        if (visitHistories.isEmpty()) {
+            throw new OwnerUnAuthorizationException("카페의 고객이 아닙니다");
+        }
+
         Cafe cafe = cafeRepository.findById(rewardFindDto.getCafeId())
                 .orElseThrow(() -> new CafeNotFoundException("카페를 찾지 못했습니다."));
         Owner owner = ownerRepository.findById(ownerId)
