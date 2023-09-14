ee6a2fc0839beaff5143dae3b6530ef9513117fd
kangyeongmin <kym6050@hanyang.ac.kr>
2023-09-14 13:04:22 +0900

    refactor: select로 useQuery data 추출해서 사용

--- frontend/src/pages/Admin/EnterPhoneNumber/hooks/useGetCustomer.ts
+++ frontend/src/pages/Admin/EnterPhoneNumber/hooks/useGetCustomer.ts
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCustomer } from '../../../../api/get';
+import { PHONE_NUMBER_LENGTH } from '../../../../constants';
 import { CustomerPhoneNumberRes } from '../../../../types/api/response';
+import { CustomerPhoneNumber } from '../../../../types/domain/customer';
 import { removeHyphen } from '../../../../utils';
 
 const useGetCustomer = (phoneNumber: string) =>
-  useQuery<CustomerPhoneNumberRes>(['customer'], {
+  useQuery<CustomerPhoneNumberRes, Error, CustomerPhoneNumber[]>(['customer'], {
     queryFn: () => getCustomer({ params: { phoneNumber: removeHyphen(phoneNumber) } }),
-    enabled: phoneNumber.length === 13,
+    select: (data) => data.customer,
+    enabled: phoneNumber.length === PHONE_NUMBER_LENGTH,
   });
 
 export default useGetCustomer;
