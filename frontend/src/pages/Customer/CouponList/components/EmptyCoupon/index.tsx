1345704fd9cea6588ba3f016ffae1eba0654e300
regularPark <srk99279@naver.com>
2023-09-15 16:37:34 +0900

    feat: 쿠폰이 없을 때 표시할 컴포넌트 생성

---
+++ frontend/src/pages/Customer/CouponList/components/EmptyCoupon/index.tsx
@@ -0,0 +1,13 @@
+import { InfoContainer } from '../../style';
+import Header from '../Header';
+
+const EmptyCoupon = () => {
+  return (
+    <>
+      <Header />
+      <InfoContainer>보유하고 있는 쿠폰이 없습니다.</InfoContainer>
+    </>
+  );
+};
+
+export default EmptyCoupon;
