126d569f0d98cfce43507b41d5d17b160e297eca
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-09-15 15:30:31 +0900

    [BE] feat: 사장 회원가입/로그인 API (테스트 용도) (#682)
    * feat: Owner 가입 API 생성 (테스트 용도)
    
    * test: 사장 가입 API 테스트
    
    * chore: 패키지 변경

---
+++ backend/src/test/java/com/stampcrush/backend/acceptance/step/ManagerJoinStep.java
@@ -0,0 +1,30 @@
+package com.stampcrush.backend.acceptance.step;
+
+import com.stampcrush.backend.auth.api.request.OAuthRegisterOwnerCreateRequest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+public class ManagerJoinStep {
+
+    public static String 카페_사장_회원_가입_요청하고_액세스_토큰_반환(OAuthRegisterOwnerCreateRequest request) {
+        ExtractableResponse<Response> response = 카페_사장_회원_가입_요청(request);
+        return response.jsonPath().getString("accessToken");
+    }
+
+    public static ExtractableResponse<Response> 카페_사장_회원_가입_요청(OAuthRegisterOwnerCreateRequest request) {
+        return given()
+                .log().all()
+                .contentType(JSON)
+                .body(request)
+
+                .when()
+                .post("/api/login/owner/register/test/token")
+
+                .then()
+                .log().all()
+                .extract();
+    }
+}
