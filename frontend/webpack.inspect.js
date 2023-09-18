0c83b7e30b75fa6386fe72ee184208a7f393c9a5
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-09-18 17:36:25 +0900

    [FE] refactor: webpack 설정을 개선한다. (#679)
    * build: webpack 설정에서 eslint를 무시
    
    * build: 프로덕션에서 필요없는 소스맵 제거 및 dev 소스맵 변경
    
    - 웹팩에서 추천하는 eval-cheap-module-source-map 사용
    
    * build: 검사해야하는 모듈만 include 명시
    
    * build: 캐시를 위한 webpack 빌드 시 contenthash 설정 (#593)
    
    * chore: webpack bundle analyzer 설치
    
    * build: 중복되는 ts 속성제거
    
    * build: 사용하지 않는 babel 관련 설정 제거
    
    * build: bundle analyzer 적용
    
    * build: production 빌드 내용을 로컬에서 확인하는 config 추가

--- frontend/webpack.inspect.js
+++ frontend/webpack.inspect.js
@@ -1,20 +1,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-  },
+  mode: 'production',
   plugins: [new BundleAnalyzerPlugin()],
 });
