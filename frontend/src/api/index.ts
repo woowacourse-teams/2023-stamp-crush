3bf30502cf8c337ccd085ebf0f50c70e61982fa3
2yunseong <dbsdltjd3701@naver.com>
2023-09-19 16:06:43 +0900

    feat: 토큰이 만료되었을 시, 토큰을 비워주는 로직 작성

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -9,7 +9,14 @@ const request = async (path: string, init?: RequestInit) => {
     },
   });
 
-  if (!response.ok) throw new Error(response.status.toString());
+  if (!response.ok) {
+    if (response.status === 401) {
+      // TODO: 현재는 로그인에 실패하면 모든 토큰을 비워버림. 추후 고객 페이지 요청과 사장님 페이지의 요청의 에러핸들링을 분리해야함.
+      localStorage.setItem('login-token', '');
+      localStorage.setItem('admin-login-token', '');
+    }
+    throw new Error(response.status.toString());
+  }
   return response;
 };
 
