e4fce6fc1fbb2728141e2f8d4248c2d5308ce313
kangyeongmin <kym6050@hanyang.ac.kr>
2023-09-19 13:51:54 +0900

    refactor: 고객 정렬 옵션 타입 단언 제거 및 리팩토링

--- frontend/src/pages/Admin/CustomerList/hooks/useGetCustomers.ts
+++ frontend/src/pages/Admin/CustomerList/hooks/useGetCustomers.ts
@@ -5,7 +5,11 @@ import { CustomersRes } from '../../../../types/api/response';
 import { Customer } from '../../../../types/domain/customer';
 import { Option } from '../../../../types/utils';
 
-const useGetCustomers = (cafeId: number, orderOption: Option) => {
+interface CustomerOrderOption extends Omit<Option, 'key'> {
+  key: keyof Customer;
+}
+
+const useGetCustomers = (cafeId: number, orderOption: CustomerOrderOption) => {
   return useQuery<CustomersRes, Error, Customer[]>({
     queryKey: ['customers'],
     queryFn: () =>
@@ -16,9 +20,9 @@ const useGetCustomers = (cafeId: number, orderOption: Option) => {
       }),
     select: (data) =>
       data.customers.sort((a, b) => {
-        if (a[orderOption.key as keyof Customer] === b[orderOption.key as keyof Customer])
+        if (a[orderOption.key] === b[orderOption.key])
           return a['nickname'] > b['nickname'] ? 1 : -1;
-        return a[orderOption.key as keyof Customer] < b[orderOption.key as keyof Customer] ? 1 : -1;
+        return a[orderOption.key] < b[orderOption.key] ? 1 : -1;
       }),
     enabled: cafeId !== INVALID_CAFE_ID,
   });
