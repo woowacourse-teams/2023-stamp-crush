f80124ad7ac2dbedd90bb32fab06744724d0c084
kangyeongmin <kym6050@hanyang.ac.kr>
2023-09-19 16:35:38 +0900

    refactor: 전화번호 길이 상수화

--- frontend/src/pages/Admin/InputPhoneNumber/hooks/useGetCustomerRegisterType.ts
+++ frontend/src/pages/Admin/InputPhoneNumber/hooks/useGetCustomerRegisterType.ts
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCustomerRegisterType } from '../../../../api/get';
+import { PHONE_NUMBER_LENGTH } from '../../../../constants';
+import { removeHyphen } from '../../../../utils';
 
 const useGetCustomerRegisterType = (phoneNumber: string) => {
   return useQuery(['customerRegisterType'], {
-    queryFn: () => getCustomerRegisterType({ params: { phoneNumber } }),
+    queryFn: () => getCustomerRegisterType({ params: { phoneNumber: removeHyphen(phoneNumber) } }),
     select: (data) => data.customers,
-    enabled: phoneNumber.length !== 13,
+    enabled: phoneNumber.length === PHONE_NUMBER_LENGTH,
   });
 };
 
