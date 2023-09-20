99eb24dfeb3b8c49c02ceb9ed17707f80800703f
박정규 <srk99279@naver.com>
2023-09-20 20:06:16 +0900

    fix: 배포 중 오류 (타입,tsx) 트러블 슈팅 (#729)
    (cherry picked from commit 21aaa3e9f4b4a6012536f3d1d6e1ee0419403e96)

--- frontend/src/components/LoadingSpinner/CustomerLoading/index.tsx
+++ frontend/src/components/LoadingSpinner/CustomerLoading/index.tsx
@@ -1,4 +1,4 @@
-import { FaStamp } from '@react-icons/all-files/fa/fastamp';
+import { FaStamp } from '@react-icons/all-files/fa/FaStamp';
 import { LoadingContainer } from './style';
 
 const CustomerLoading = () => {
