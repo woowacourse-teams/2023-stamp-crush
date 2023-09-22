d5d316745e07999e4fd3fc0e4d9df5d2ccdf0012
gitchan <eunkeeeee@gmail.com>
2023-09-22 16:47:11 +0900

    fix: URI 매핑 수정

--- backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorCancelMembershipController.java
+++ backend/src/main/java/com/stampcrush/backend/api/visitor/profile/VisitorCancelMembershipController.java
@@ -5,10 +5,12 @@ import com.stampcrush.backend.config.resolver.CustomerAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class VisitorCancelMembershipController {
 
     private final VisitorProfilesCommandService visitorProfilesCommandService;
