f101176d0e4ebe9fedcc5644e5bd17593844e024
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-09-22 15:14:17 +0900

    [BE] test: Manager.customer , Manager.sample, Visitor.coupon 인수테스트 리팩터링 (#701)
    * refactor: 회원가입 로직 API로 대체
    
    * fix: 임시 회원 가입 요청 API 변경
    
    * [BE] test: 테스트에서 사용하는 API 최신 스펙에 따라 변경 (#700)
    
    * fix: 임시 회원 가입 요청 API 변경
    
    * refactor: 메서드 Deprecated 어노테이션 추가
    
    * refactor: 주석 추가
    
    * refactor: 사용하지 않는 메서드에 Deprecated 어노테이션 추가
    
    * refactor: 카페 생성 요청 매개변수 accessToken 으로 수정
    
    ---------
    
    Co-authored-by: yenawee <yaena0319@naver.com>
    
    * refactor: 회원가입 로직 API로 대체
    
    * fix: 임시 회원 가입 요청 API 변경
    
    * refactor: Manager Customer 패키지의 테스트 find, command로 분리
    
    * refactor: ManagerSampleCouponFindStep 분리
    
    * refactor: 카페 쿠폰 정책 수정 요청 step 시그니처 변경
    
    * refactor: 쿠폰 생성 요청 step 변경
    
    * refactor: 고객의 쿠폰 조회 인수테스트에서 새로운 step을 사용하도록 변경
    
    * refactor: 고객의 쿠폰 삭제 인수테스트에서 새로운 step을 사용하도록 변경
    
    * refactor: 임시 고객 생성 세부 로직 변경
    
    * refactor: 불필요한 변수 삭제
    
    * refactor: TODO 작성
    
    * refactor: 쿠폰 조회 메서드 교체 (정렬 기능 제거)
    
    * refactor: TODO 주석 추가
    
    ---------
    
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/repository/coupon/CouponRepository.java
+++ backend/src/main/java/com/stampcrush/backend/repository/coupon/CouponRepository.java
@@ -13,13 +13,17 @@ import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
+    // TODO: coupon_policy, coupon_design을 같이 가져오자
     @Query("select distinct cp from Coupon cp left join fetch cp.stamps join fetch cp.cafe join fetch cp.customer where cp.cafe = :cafe")
     List<Coupon> findByCafe(@Param("cafe") Cafe cafe);
 
     List<Coupon> findByCafeAndCustomerAndStatus(@Param("cafe") Cafe cafe, @Param("customer") Customer customer, @Param("status") CouponStatus status);
 
+    // TODO: 즐겨찾기가 붙어있는지 등에 따라 정렬하기로 했었는데, 현재 로직에서는 정렬을 하고 있지 않아서, 아래 메서드로 대체함
     @Query("SELECT c FROM Coupon c LEFT JOIN Favorites f ON c.cafe.id = f.cafe.id WHERE c.customer = :customer AND c.status = :status ORDER BY f.isFavorites DESC, c.expiredDate ASC")
     List<Coupon> findFilteredAndSortedCoupons(@Param("customer") Customer customer, @Param("status") CouponStatus status);
+
+    List<Coupon> findCouponsByCustomerAndStatus(Customer customer, CouponStatus couponStatus);
 
     Optional<Coupon> findByIdAndCustomerId(Long id, Long customerId);
 }
