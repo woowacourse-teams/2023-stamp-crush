d7294068152bb1c702e6e6d1d4685ceb7629b127
kangyeongmin <kym6050@hanyang.ac.kr>
2023-09-22 10:29:01 +0900

    fix: 충돌 해결

--- frontend/src/pages/Customer/InputPhoneNumber/index.tsx
+++ frontend/src/pages/Customer/InputPhoneNumber/index.tsx
@@ -20,7 +20,7 @@ const InputPhoneNumber = () => {
 
   return (
     <Form onSubmit={submitPhoneNumber}>
-      <img src={LoginLogo} alt="스탬프크러쉬 로고" width={200} />
+      <LoginLogo width={200} />
       <Input
         id="input-phone-number"
         label={'전화번호'}
