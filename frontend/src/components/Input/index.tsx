4e6884af5953199b1074adc79e3234aa78ff2497
regularPark <srk99279@naver.com>
2023-09-24 19:21:41 +0900

    refactor: 핫픽스한 `Input`의 싱크를 맞춘다

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, HTMLInputTypeAttribute, InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import { BaseInput, InputContainer, InputWidth, Label, LabelWrapper, Required } from './style';
 
 const REQUIRED = '*' as const;
@@ -6,23 +6,43 @@ const REQUIRED = '*' as const;
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label?: string;
-  type?: HTMLInputTypeAttribute;
   width?: InputWidth;
-  placeholder?: string;
-  maxLength?: number;
-  required?: boolean;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return (
-    <InputContainer>
-      <LabelWrapper>
-        <Label htmlFor={props.id}>{props.label}</Label>
-        {props.required && <Required>{REQUIRED}</Required>}
-      </LabelWrapper>
-      <BaseInput ref={ref} $width={props.width} {...props} />
-    </InputContainer>
-  );
-});
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      id,
+      label,
+      type,
+      width = 'fill',
+      placeholder,
+      maxLength,
+      required = false,
+      ...props
+    }: InputProps,
+    ref,
+  ) => {
+    return (
+      <InputContainer>
+        <LabelWrapper>
+          <Label htmlFor={id}>{label}</Label>
+          {required && <Required>{REQUIRED}</Required>}
+        </LabelWrapper>
+        <BaseInput
+          ref={ref}
+          id={id}
+          $width={width}
+          type={type}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          required={required}
+          autoComplete="off"
+          {...props}
+        />
+      </InputContainer>
+    );
+  },
+);
 
 Input.displayName = 'Input';
