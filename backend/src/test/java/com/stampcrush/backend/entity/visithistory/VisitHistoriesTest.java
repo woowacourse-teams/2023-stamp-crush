63327b478aab1900edc830bb096b113e4c2b94b8
Youngho <cyh6099@gmail.com>
2023-09-25 16:54:38 +0900

    feat: 최근 방문 일자를 계산하는 로직 구현

--- backend/src/test/java/com/stampcrush/backend/entity/visithistory/VisitHistoriesTest.java
+++ backend/src/test/java/com/stampcrush/backend/entity/visithistory/VisitHistoriesTest.java
@@ -26,4 +26,21 @@ class VisitHistoriesTest {
         LocalDateTime expected = LocalDateTime.of(2023, 5, 12, 4, 30);
         Assertions.assertThat(firstVisitDateResult).isEqualTo(expected);
     }
+
+    @Test
+    void 최근_방문_일자를_계산한다() {
+        // given
+        LocalDateTime firstVisitDate = LocalDateTime.of(2023, 5, 12, 4, 30);
+        LocalDateTime secondVisitDate = LocalDateTime.of(2023, 6, 12, 4, 30);
+        VisitHistory firstVisitHistory = new VisitHistory(firstVisitDate, null, null, null, 1);
+        VisitHistory secondVisitHistory = new VisitHistory(secondVisitDate, null, null, null, 1);
+
+        // when
+        VisitHistories visitHistories = new VisitHistories(List.of(firstVisitHistory, secondVisitHistory));
+        LocalDateTime recentVisitDateResult = visitHistories.getRecentVisitDate();
+
+        // then
+        LocalDateTime expected = LocalDateTime.of(2023, 6, 12, 4, 30);
+        Assertions.assertThat(recentVisitDateResult).isEqualTo(expected);
+    }
 }
