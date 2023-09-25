0bc81a9ae0fca9b22550dd89ea44800de0a4647c
regularPark <srk99279@naver.com>
2023-09-25 23:20:03 +0900

    feat: `AdminLoginForm` 컴포넌트 생성

---
+++ frontend/src/pages/Admin/AdminLogin/components/AdminLoginForm.tsx
@@ -0,0 +1,41 @@
+import useAdminLogin from '../hooks/useAdminLogin';
+import { LoginWrapper, LoginForm } from '../style';
+import { AiOutlineUser } from '@react-icons/all-files/ai/AiOutlineUser';
+import { AiOutlineLock } from '@react-icons/all-files/ai/AiOutlineLock';
+import { Input } from '../../../../components/Input';
+import { Spacing } from '../../../../style/layout/common';
+import Button from '../../../../components/Button';
+
+const AdminLoginForm = () => {
+  const { isFocusedId, isFocusedPw, handleBlur, handleFocus, loginAdmin } = useAdminLogin();
+
+  return (
+    <LoginForm onSubmit={loginAdmin}>
+      <LoginWrapper>
+        <AiOutlineUser size={20} color={isFocusedId ? 'black' : '#aaa'} />
+        <Input
+          id="admin-login"
+          placeholder="아이디"
+          minLength={5}
+          onFocus={handleFocus('ID')}
+          onBlur={handleBlur}
+        />
+      </LoginWrapper>
+      <Spacing $size={8} />
+      <LoginWrapper>
+        <AiOutlineLock size={20} color={isFocusedPw ? 'black' : '#aaa'} />
+        <Input
+          id="admin-password"
+          type="password"
+          placeholder="비밀번호"
+          onFocus={handleFocus('PW')}
+          onBlur={handleBlur}
+        />
+      </LoginWrapper>
+      <Spacing $size={12} />
+      <Button size="large">로그인</Button>
+    </LoginForm>
+  );
+};
+
+export default AdminLoginForm;
