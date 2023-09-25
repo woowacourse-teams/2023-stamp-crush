15564d3161eab46d02c976382f20e58202e25359
gitchan <eunkeeeee@gmail.com>
2023-09-25 15:35:52 +0900

    feature: Access Token 만료시간 24시간으로 설정

--- backend/src/main/java/com/stampcrush/backend/auth/application/util/AuthTokensGenerator.java
+++ backend/src/main/java/com/stampcrush/backend/auth/application/util/AuthTokensGenerator.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class AuthTokensGenerator {
 
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;       // 24시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     private final JwtTokenProvider jwtTokenProvider;
