6df331f9bf9fe06da0868e30c10eb3bd29681836
Youngho <cyh6099@gmail.com>
2023-09-25 23:00:15 +0900

    feat: 사장 모드 고객 타입 별 목록 조회 컨트롤러 작성

--- backend/src/main/java/com/stampcrush/backend/api/manager/coupon/ManagerCouponFindApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/manager/coupon/ManagerCouponFindApiController.java
@@ -10,7 +10,11 @@ import com.stampcrush.backend.application.manager.coupon.dto.CustomerAccumulatin
 import com.stampcrush.backend.config.resolver.OwnerAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -27,6 +31,20 @@ public class ManagerCouponFindApiController {
             @PathVariable("cafeId") Long cafeId
     ) {
         List<CafeCustomerFindResultDto> coupons = managerCouponFindService.findCouponsByCafe(owner.getId(), cafeId);
+        List<CafeCustomerFindResponse> cafeCustomerFindResponses = coupons.stream()
+                .map(CafeCustomerFindResponse::from)
+                .toList();
+
+        return ResponseEntity.ok(new CafeCustomersFindResponse(cafeCustomerFindResponses));
+    }
+
+    @GetMapping(value = "/cafes/{cafeId}/customers", params = "status")
+    public ResponseEntity<CafeCustomersFindResponse> findCustomersByCafeAndStatus(
+            OwnerAuth owner,
+            @PathVariable("cafeId") Long cafeId,
+            @RequestParam("status") String status
+    ) {
+        List<CafeCustomerFindResultDto> coupons = managerCouponFindService.findCouponsByCafeAndCustomerType(owner.getId(), cafeId, status);
         List<CafeCustomerFindResponse> cafeCustomerFindResponses = coupons.stream()
                 .map(CafeCustomerFindResponse::from)
                 .toList();
--- backend/src/test/java/com/stampcrush/backend/api/manager/coupon/ManagerCouponFindApiControllerTest.java
+++ backend/src/test/java/com/stampcrush/backend/api/manager/coupon/ManagerCouponFindApiControllerTest.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +38,20 @@ public class ManagerCouponFindApiControllerTest extends ControllerSliceTest {
 
         // then
         mockMvc.perform(get(API_PREFIX + "/cafes/{cafeId}/customers", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 카페에_방문한_고객들의_타입_별로_정보를_조회한다() throws Exception {
+        // given, when
+        CafeCustomerFindResultDto customerInfo1 = new CafeCustomerFindResultDto(1L, "name1", 5, 0, 3, LocalDateTime.now(), false, 10, null);
+        CafeCustomerFindResultDto customerInfo2 = new CafeCustomerFindResultDto(2L, "name2", 6, 0, 6, LocalDateTime.now(), false, 10, null);
+        given(managerCouponFindService.findCouponsByCafeAndCustomerType(anyLong(), anyLong(), anyString()))
+                .willReturn(List.of(customerInfo1, customerInfo2));
+
+        // then
+        mockMvc.perform(get(API_PREFIX + "/cafes/{cafeId}/customers", 1L)
+                        .param("status", "temporary"))
                 .andExpect(status().isOk());
     }
 
