ee9f4272fd503d2a58213f4b9d5cd4ffc2f9b07a
2yunseong <dbsdltjd3701@naver.com>
2023-09-26 16:35:21 +0900

    feat: 회원 탈퇴 mutation query 추가

---
+++ frontend/src/pages/Customer/Cancellation/hooks/useDeleteCustomer.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import { deleteCustomer } from '../../../../api/delete';
+import { useNavigate } from 'react-router-dom';
+import { ROUTER_PATH } from '../../../../constants';
+
+const useDeleteCustomer = () => {
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: () => deleteCustomer(),
+    onSuccess: () => {
+      alert('회원 탈퇴에 성공했습니다.');
+      // TODO: 완전한 로그아웃 및 페이지 이동
+      localStorage.setItem('login-token', '');
+      navigate(ROUTER_PATH.login);
+    },
+    onError: () => {
+      throw new Error('회원 탈퇴에 실패했습니다.');
+    },
+  });
+};
+
+export default useDeleteCustomer;
