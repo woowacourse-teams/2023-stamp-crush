87a48f5f8eb9bb19d9e4bc3ceb14f36184862990
gitchan <eunkeeeee@gmail.com>
2023-09-27 14:58:33 +0900

    fix: 테스트 빈 에러 해결
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/test/java/com/stampcrush/backend/api/ControllerSliceTest.java
+++ backend/src/test/java/com/stampcrush/backend/api/ControllerSliceTest.java
@@ -2,6 +2,7 @@ package com.stampcrush.backend.api;
 
 import com.stampcrush.backend.auth.application.util.AuthTokensGenerator;
 import com.stampcrush.backend.auth.application.util.JwtTokenProvider;
+import com.stampcrush.backend.auth.repository.BlackListRepository;
 import com.stampcrush.backend.common.KorNamingConverter;
 import com.stampcrush.backend.repository.cafe.CafeRepository;
 import com.stampcrush.backend.repository.user.CustomerRepository;
@@ -18,6 +19,9 @@ public abstract class ControllerSliceTest {
 
     @Autowired
     public MockMvc mockMvc;
+
+    @MockBean
+    public BlackListRepository blackListRepository;
 
     @MockBean
     public OwnerRepository ownerRepository;
