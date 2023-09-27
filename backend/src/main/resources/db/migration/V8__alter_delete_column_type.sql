a5e622123d9d272e1a2a78ceb0ba1accdd27509c
yenawee <yaena0319@naver.com>
2023-09-27 13:35:54 +0900

    [BE] fix: deleted column 의 type 을 bit(1) 로 변경하는 스크립트 추가 (#790)

---
+++ backend/src/main/resources/db/migration/V8__alter_delete_column_type.sql
@@ -0,0 +1,5 @@
+ALTER TABLE customer MODIFY deleted bit (1);
+ALTER TABLE coupon_design MODIFY deleted bit (1);
+ALTER TABLE coupon_policy MODIFY deleted bit (1);
+ALTER TABLE coupon_stamp_coordinate MODIFY deleted bit (1);
+ALTER TABLE stamp MODIFY deleted bit (1);
