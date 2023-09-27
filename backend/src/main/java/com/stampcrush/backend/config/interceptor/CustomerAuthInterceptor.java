d9d1d8bdc732d3212ec19fd0057db697d6186d52
gitchan <eunkeeeee@gmail.com>
2023-09-27 14:20:23 +0900

    feat: 인증 시 Refresh Token 추출
    Co-authored-by: yenawee <yaena0319@naver.com>

--- backend/src/main/java/com/stampcrush/backend/config/interceptor/CustomerAuthInterceptor.java
+++ backend/src/main/java/com/stampcrush/backend/config/interceptor/CustomerAuthInterceptor.java
@@ -1,9 +1,12 @@
 package com.stampcrush.backend.config.interceptor;
 
+import com.stampcrush.backend.auth.api.response.AuthTokensResponse;
 import com.stampcrush.backend.auth.application.util.AuthTokensGenerator;
 import com.stampcrush.backend.auth.application.util.BearerParser;
+import com.stampcrush.backend.auth.repository.BlackListRepository;
 import com.stampcrush.backend.exception.UnAuthorizationException;
 import com.stampcrush.backend.repository.user.CustomerRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class CustomerAuthInterceptor implements HandlerInterceptor {
 
     private final CustomerRepository customerRepository;
+    private final BlackListRepository blackListRepository;
     private final AuthTokensGenerator authTokensGenerator;
 
     @Override
@@ -27,12 +31,39 @@ public class CustomerAuthInterceptor implements HandlerInterceptor {
 
         String accessToken = BearerParser.parseAuthorization(authorization);
         if (!authTokensGenerator.isValidToken(accessToken)) {
+
+            String refreshToken = getRefreshToken(request);
+            if (blackListRepository.isValidRefreshToken(refreshToken)) {
+                // TODO: Access Token, Refresh Token 재발급
+                Long customerId = getCustomerId(accessToken);
+                AuthTokensResponse newTokens = authTokensGenerator.generate(customerId);
+                request.setAttribute("Authorization", newTokens.getAccessToken());
+            }
+            // TODO: 인증 안됨
+
             throw new UnAuthorizationException("인증할 수 없습니다.");
         }
 
-        Long customerId = authTokensGenerator.extractMemberId(accessToken);
+        Long customerId = getCustomerId(accessToken);
         customerRepository.findById(customerId)
                 .orElseThrow(() -> new UnAuthorizationException("인증할 수 없습니다."));
         return true;
+    }
+
+    private Long getCustomerId(String accessToken) {
+        return authTokensGenerator.extractMemberId(accessToken);
+    }
+
+    private String getRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            throw new UnAuthorizationException("Cookie가 존재하지 않습니다");
+        }
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refreshToken")) {
+                return cookie.getValue();
+            }
+        }
+        throw new UnAuthorizationException("Refresh Token이 존재하지 않습니다");
     }
 }
