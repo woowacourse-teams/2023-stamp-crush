cb15220f2786ca3e42e1e1d54d03739a3d6845fe
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-09-27 12:04:49 +0900

    [BE] fix: Flyway 스크립트 수정 (#788)

--- backend/src/main/resources/db/migration/V6__delete_customer.sql
+++ backend/src/main/resources/db/migration/V6__delete_customer.sql
@@ -1,14 +1,2 @@
 ALTER TABLE customer
     ADD deleted BOOLEAN DEFAULT false;
-
-ALTER TABLE coupon_design
-    ADD deleted BOOLEAN DEFAULT false;
-
-ALTER TABLE coupon_policy
-    ADD deleted BOOLEAN DEFAULT false;
-
-ALTER TABLE coupon_stamp_coordinate
-    ADD deleted BOOLEAN DEFAULT false;
-
-ALTER TABLE stamp
-    ADD deleted BOOLEAN DEFAULT false;
