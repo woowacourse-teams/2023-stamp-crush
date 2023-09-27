9833a3229dfb8e347c35b33c3416da2ff95c3125
regularPark <srk99279@naver.com>
2023-09-27 14:05:02 +0900

    refactor: 훅을 사용해 분리

--- frontend/src/pages/Admin/SignUp/index.tsx
+++ frontend/src/pages/Admin/SignUp/index.tsx
@@ -1,59 +1,23 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
 import { LoginLogo } from '../../../assets';
 import Button from '../../../components/Button';
 import { Input } from '../../../components/Input';
 import { Spacing } from '../../../style/layout/common';
 import { ErrorCaption, InputWrapper, SignUpForm, SignUpTemplate } from './style';
-import { ID_REGEX, PW_REGEX } from '../../../constants';
+import useSignUp from './hooks/useSignUp';
 
 const SignUp = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [checkedPassword, setCheckedPassword] = useState('');
-
-  const [isValidateId, setIsValidateId] = useState(false);
-  const [isValidatePw, setIsValidatePw] = useState(false);
-
-  const signUp = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (password !== checkedPassword) {
-      alert('비밀번호가 불일치합니다. 동일한 비밀번호를 입력해주세요.');
-      return;
-    }
-
-    if (!ID_REGEX.test(id)) {
-      alert('아이디는 영문자와 숫자만 허용됩니다.');
-      return;
-    }
-
-    if (!PW_REGEX.test(password)) {
-      alert(
-        '비밀번호는 영문자, 숫자 및 특수문자 조합이어야 하며, 4자 이상 20자 이하로 입력되어야 합니다.',
-      );
-      return;
-    }
-  };
-
-  const inputCheckedPassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setCheckedPassword(e.target.value);
-  };
-
-  const checkPassword = () => {
-    return password === checkedPassword;
-  };
-
-  const validateId = (e: ChangeEvent<HTMLInputElement>) => {
-    setId(e.target.value);
-    id.length > 0 && !ID_REGEX.test(id) ? setIsValidateId(true) : setIsValidateId(false);
-  };
-
-  const validatePw = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-    password.length > 0 && !PW_REGEX.test(password)
-      ? setIsValidatePw(true)
-      : setIsValidatePw(false);
-  };
+  const {
+    loginId,
+    password,
+    checkedPassword,
+    validateId,
+    validatePw,
+    isValidId,
+    isValidPw,
+    checkPassword,
+    inputCheckedPassword,
+    signUp,
+  } = useSignUp();
 
   return (
     <SignUpTemplate>
@@ -65,17 +29,19 @@ const SignUp = () => {
             label="아이디"
             minLength={5}
             maxLength={20}
+            value={loginId}
             onChange={validateId}
             placeholder="아이디를 입력해주세요."
             required={true}
           />
-          {isValidateId && <ErrorCaption>아이디는 영문과 숫자로만 작성해주세요.</ErrorCaption>}
+          {isValidId && <ErrorCaption>영문과 숫자로만 작성해주세요(5~20자).</ErrorCaption>}
         </InputWrapper>
         <InputWrapper>
           <Input
             id="admin-pw"
             label="비밀번호"
             type="password"
+            value={password}
             minLength={8}
             maxLength={30}
             onChange={validatePw}
@@ -84,9 +50,9 @@ const SignUp = () => {
             placeholder="비밀번호를 입력해주세요."
             required={true}
           />
-          {isValidatePw && (
+          {isValidPw && (
             <ErrorCaption>
-              비밀번호는 영문과 숫자, 특수기호(!@#$%^*+=-)의 조합으로 작성해주세요.
+              영문과 숫자, 특수기호(!@#$%^*+=-)의 조합으로 작성해주세요(8~30자).
             </ErrorCaption>
           )}
         </InputWrapper>
@@ -95,6 +61,7 @@ const SignUp = () => {
             id="admin-pw-check"
             label="비밀번호 확인"
             type="password"
+            value={checkedPassword}
             minLength={8}
             maxLength={30}
             onChange={inputCheckedPassword}
