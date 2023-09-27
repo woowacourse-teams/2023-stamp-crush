8447022377db8453f2e210a628dec3e3234340df
gitchan <eunkeeeee@gmail.com>
2023-09-27 13:57:08 +0900

    feat: BlackList 엔티티 및 레포지토리 생성
    Co-authored-by: yenawee <yaena0319@naver.com>

---
+++ backend/src/test/java/com/stampcrush/backend/auth/repository/BlackListRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.stampcrush.backend.auth.repository;
+
+import com.stampcrush.backend.auth.entity.BlackList;
+import com.stampcrush.backend.common.KorNamingConverter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@KorNamingConverter
+@DataJpaTest
+class BlackListRepositoryTest {
+
+    @Autowired
+    private BlackListRepository blackListRepository;
+
+    @Test
+    void 이미_존재하는_리프레쉬_토큰은_false를_반환한다() {
+        String refreshToken = "refresh_token";
+        blackListRepository.save(new BlackList(refreshToken));
+        boolean isValidRefreshToken = blackListRepository.isValidRefreshToken(refreshToken);
+        assertFalse(isValidRefreshToken);
+    }
+}
