2db0d4763efbbbc57de1144faee7a01c1432038d
youngh0 <cyh6099@gmail.com>
2023-10-03 23:11:35 +0900

    [BE] feat: 트랜잭션 어노테이션 추가 (#818)

--- backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorCancelMembershipCommandService.java
+++ backend/src/main/java/com/stampcrush/backend/application/visitor/profile/VisitorCancelMembershipCommandService.java
@@ -13,11 +13,13 @@ import com.stampcrush.backend.repository.user.CustomerRepository;
 import com.stampcrush.backend.repository.visithistory.VisitHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class VisitorCancelMembershipCommandService {
 
