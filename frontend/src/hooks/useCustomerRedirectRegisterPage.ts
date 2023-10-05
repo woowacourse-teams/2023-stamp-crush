3d5bdc72cd082bbbcd38f2d87974d149959d9dae
regularPark <srk99279@naver.com>
2023-10-05 10:37:41 +0900

    refactor: redirect 훅 수정

--- frontend/src/hooks/useCustomerRedirectRegisterPage.ts
+++ frontend/src/hooks/useCustomerRedirectRegisterPage.ts
@@ -1,20 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useCustomerProfile } from './useCustomerProfile';
 import { ROUTER_PATH } from '../constants';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const useCustomerRedirectRegisterPage = () => {
   const navigate = useNavigate();
   const { customerProfile, status } = useCustomerProfile();
+  const [hasPhoneNumber, setHasPhoneNumber] = useState(false);
 
   useEffect(() => {
+    !customerProfile?.profile.phoneNumber ? setHasPhoneNumber(false) : setHasPhoneNumber(true);
+  }, [customerProfile]);
+
+  const redirectCustomerWithoutPhoneNumber = () => {
     if (!customerProfile?.profile.phoneNumber && status === 'success') {
       alert('전화번호 등록 후 사용해주세요.');
       navigate(ROUTER_PATH.inputPhoneNumber);
     }
-  }, [customerProfile]);
+  };
 
-  return { customerProfile };
+  return { redirectCustomerWithoutPhoneNumber, customerProfile, hasPhoneNumber };
 };
 
 export default useCustomerRedirectRegisterPage;
