4b0970ca7698af189da387e84e0cfbeb4847e401
yenawee <yaena0319@naver.com>
2023-10-11 11:32:36 +0900

    healthcheckcontroller

--- backend/src/main/java/com/stampcrush/backend/api/HealthCheckApiController.java
+++ backend/src/main/java/com/stampcrush/backend/api/HealthCheckApiController.java
@@ -1,13 +1,2 @@
-package com.stampcrush.backend.api;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HealthCheckApiController {
-
-    @GetMapping("/health-check")
-    public String getHealthCheck() {
-        return "up";
-    }
+package com.stampcrush.backend.api;public class HealthCheckApiController {
 }
