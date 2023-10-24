9cacdb02c09054752c96d50bc4bd09b2d6872da7
kangyeongmin <kym6050@hanyang.ac.kr>
2023-10-24 17:51:51 +0900

    feat: 사장모드 소개 부분 구현

--- frontend/src/pages/Introduction/index.tsx
+++ frontend/src/pages/Introduction/index.tsx
@@ -1,27 +1,72 @@
-import { CustomerPreviewGif } from '../../assets';
-import { Container, Header, ServiceIntro, CustomerPreview } from './style';
+import { CustomerPreviewGif, StampcrushLogo, StampcrushWhiteLogo } from '../../assets';
+import {
+  Container,
+  ServiceIntro,
+  CustomerPreview,
+  Header,
+  Button,
+  ButtonContainer,
+  ApplyButton,
+  OwnerIntro,
+  OwnerPreviewContainer,
+  OwnerPreview,
+  Footer,
+} from './style';
 import { AiOutlineGithub } from '@react-icons/all-files/ai/AiOutlineGithub';
 import { AiFillYoutube } from '@react-icons/all-files/ai/AiFillYoutube';
+import { RiServiceLine } from '@react-icons/all-files/ri/RiServiceLine';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { OwnerPreview2Png, OwnerPreview3Png, OwnerPreview4Png } from '../../assets/png';
 
 const Introduction = () => {
   return (
-    <Container>
-      <ServiceIntro>
-        <span>STAMP CRUSH</span>
-        <h1>
-          이제 <br /> 종이쿠폰의 시대는 <br />
-          끝났다.
-        </h1>
-        <p>
-          온라인 쿠폰 적립 및 관리 플랫폼, 스탬프크러쉬입니다.
-          <br /> 카페사장모드, 고객모드 두가지를 운영해요. 더 이상, 꼬깃꼬깃한 종이쿠폰 주고 받지
-          마시고 <br /> 종이쿠폰을 간편하게 적립하고 관리해보세요.
-        </p>
-      </ServiceIntro>
-      <CustomerPreview src={CustomerPreviewGif} alt="서비스 고객모드 미리보기" />
-      <AiOutlineGithub />
-      <AiFillYoutube />
-    </Container>
+    <ParallaxProvider>
+      <Container>
+        <Header>
+          <StampcrushLogo />
+          <ButtonContainer>
+            <Button>고객님 로그인</Button>
+            <Button $isFilled>사장님 로그인</Button>
+          </ButtonContainer>
+        </Header>
+        <ApplyButton>
+          서비스 상담 문의
+          <RiServiceLine size={24} />
+        </ApplyButton>
+        <ServiceIntro>
+          <span>STAMP CRUSH</span>
+          <h1>
+            이제 <br /> 종이쿠폰의 시대는 <br />
+            <span>끝났다</span>
+          </h1>
+          <p>
+            온라인 쿠폰 적립 및 관리 플랫폼, 스탬프크러쉬입니다.
+            <br /> 카페사장모드, 고객모드 두가지를 운영해요. 더 이상, 꼬깃꼬깃한 종이쿠폰 주고 받지
+            마시고 <br /> 종이쿠폰을 간편하게 적립하고 관리해보세요.
+          </p>
+        </ServiceIntro>
+        <Parallax translateY={['150px', '-200px']} scale={[1, 1.2]}>
+          <CustomerPreview src={CustomerPreviewGif} alt="서비스 고객모드 미리보기" />
+        </Parallax>
+        <OwnerIntro>사용하시던 종이쿠폰 그대로</OwnerIntro>
+        <OwnerPreviewContainer>
+          <Parallax translateX={['300px', '-200px']}>
+            <OwnerPreview src={OwnerPreview2Png} alt="서비스 고객모드 미리보기" />
+          </Parallax>
+          <Parallax translateX={['300px', '-200px']}>
+            <OwnerPreview src={OwnerPreview3Png} alt="서비스 고객모드 미리보기" />
+          </Parallax>
+          <Parallax translateX={['300px', '-200px']}>
+            <OwnerPreview src={OwnerPreview4Png} alt="서비스 고객모드 미리보기" />
+          </Parallax>
+        </OwnerPreviewContainer>
+
+        <Footer>
+          <AiOutlineGithub />
+          <AiFillYoutube />
+        </Footer>
+      </Container>
+    </ParallaxProvider>
   );
 };
 
--- frontend/src/pages/Introduction/style.tsx
+++ frontend/src/pages/Introduction/style.tsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
+interface ButtonStyledProps {
+  $isFilled?: boolean;
+}
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 40px;
+  height: 78px;
+  position: fixed;
+  top: 0;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 5%, transparent);
+`;
+
+export const ButtonContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+`;
+
+export const Button = styled.button<ButtonStyledProps>`
+  padding: 13px 15px;
+  background: ${({ $isFilled }) => ($isFilled ? 'black' : 'transparent')};
+  color: ${({ $isFilled }) => ($isFilled ? 'white' : 'black')};
+  border-radius: 5px;
+  border: ${({ $isFilled }) => ($isFilled ? 'none' : '1px solid black')};
+
+  &:hover {
+    font-weight: 600;
+  }
+`;
+
 export const Container = styled.main`
   font-family: sans-serif;
   display: flex;
@@ -10,24 +45,18 @@ export const Container = styled.main`
   background: #fcf8f2;
 `;
 
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  font-size: 24px;
-  height: 30px;
-`;
-
 export const CustomerPreview = styled.img`
   width: 415px;
   border-radius: 20px;
   box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.1);
+  z-index: 2;
 `;
 
 export const ServiceIntro = styled.main`
   display: flex;
   flex-direction: column;
   text-align: center;
-  height: 30%;
+  /* height: 30%; */
 
   & > span {
     font-size: 12px;
@@ -37,16 +66,71 @@ export const ServiceIntro = styled.main`
   }
 
   & > h1 {
-    font-size: 52px;
-    font-weight: 800;
+    font-size: 56px;
+    font-weight: 700;
     margin-bottom: 40px;
-    line-height: 60px;
+    line-height: 62px;
+
+    & > span {
+      font-size: 52px;
+      font-weight: 900;
+      color: #674ea7;
+      text-emphasis-style: dot;
+    }
   }
 
   & > p {
     line-height: 21px;
-    font-size: 14px;
+    font-size: 16px;
     color: gray;
   }
-  margin: 20rem 0;
+  margin: 10rem 0 5rem 0;
+`;
+
+export const ApplyButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.1);
+  border: none;
+  background: white;
+  border-radius: 10px;
+  padding: 20px;
+  position: fixed;
+  bottom: 3rem;
+  right: 4rem;
+  font-size: 18px;
+  font-weight: 600;
+  z-index: 10;
+`;
+
+export const OwnerIntro = styled.h2`
+  display: flex;
+  text-align: center;
+  font-size: 56px;
+  font-weight: 700;
+  margin: 10rem 0 0 0;
+  line-height: 62px;
+`;
+
+export const OwnerPreviewContainer = styled.section`
+  display: flex;
+  gap: 5rem;
+  overflow: hidden;
+  margin-top: 5rem;
+  padding: 2rem 0;
+`;
+
+export const OwnerPreview = styled.img`
+  width: 600px;
+  height: 400px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 20px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 30rem;
+  background: #808080;
+  margin-top: 10rem;
 `;
