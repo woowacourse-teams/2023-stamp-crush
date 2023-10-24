45fedf6f5ae4aee16a35a3e5ac474c09aa3e56aa
2yunseong <dbsdltjd3701@naver.com>
2023-10-24 14:20:54 +0900

    refactor: 404에러 해결

--- frontend/src/pages/Admin/RewardPage/components/RewardItemList/index.tsx
+++ frontend/src/pages/Admin/RewardPage/components/RewardItemList/index.tsx
@@ -3,14 +3,20 @@ import { RewardName, RewardItemListContainer, RewardItem } from './style';
 import Button from '../../../../../components/Button';
 import useReward from '../../hooks/useReward';
 import useMutateReward from '../../hooks/useMutateReward';
+import { useNavigate } from 'react-router-dom';
 
 interface RewardItemListProps {
   cafeId: number;
   customerId: number;
 }
+
+const isNotFoundUser = (error: Error) => {
+  return error.message === '404';
+};
 const RewardItemList = ({ cafeId, customerId }: RewardItemListProps) => {
   // TODO: 내카페의 고객이 아닌 고객의 리워드를 조회할 경우 ErrorMessage를 띄어줌
-  const { data: rewardData, status: rewardStatus } = useReward(cafeId, customerId);
+  const navigate = useNavigate();
+  const reward = useReward(cafeId, customerId);
   const { mutate: mutateReward } = useMutateReward();
 
   const activateRewardButton = (rewardId: number) => {
@@ -26,21 +32,47 @@ const RewardItemList = ({ cafeId, customerId }: RewardItemListProps) => {
     });
   };
 
-  if (rewardStatus === 'error') {
-    return <div>불러오는 중 에러가 발생했습니다. 다시 시도해주세요.</div>;
+  const refreshReward = () => {
+    reward.refetch();
+  };
+
+  const goBackButton = () => {
+    navigate(-1);
+  };
+
+  if (reward.status === 'error') {
+    return (
+      <RewardItemListContainer>
+        {isNotFoundUser(reward.error) ? (
+          <>
+            <div>고객을 카페에서 찾을 수 없습니다.</div>
+            <Button onClick={goBackButton}>뒤로가기</Button>
+          </>
+        ) : (
+          <>
+            <div>에러가 발생했습니다.</div>
+            <Button onClick={refreshReward}>다시 불러오기</Button>
+          </>
+        )}
+      </RewardItemListContainer>
+    );
   }
 
-  if (rewardStatus === 'loading') {
-    return <div>고객 정보 불러오는 중...</div>;
+  if (reward.status === 'loading') {
+    return (
+      <RewardItemListContainer>
+        <div>고객 정보 불러오는 중...</div>
+      </RewardItemListContainer>
+    );
   }
 
-  if (rewardData.rewards.length === 0) {
-    return <p>보유한 리워드가 없습니다.</p>;
+  if (reward.data.rewards.length === 0) {
+    return <RewardItemListContainer>보유한 리워드가 없습니다.</RewardItemListContainer>;
   }
 
   return (
     <RewardItemListContainer>
-      {rewardData.rewards.map(({ id, name }: Reward) => (
+      {reward.data.rewards.map(({ id, name }: Reward) => (
         <RewardItem key={id}>
           <RewardName>{name}</RewardName>
           <Button onClick={() => activateRewardButton(id)}>사용</Button>
--- frontend/src/pages/Admin/RewardPage/hooks/useReward.ts
+++ frontend/src/pages/Admin/RewardPage/hooks/useReward.ts
@@ -1,15 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { INVALID_CAFE_ID } from '../../../../constants/magicNumber';
 import { getReward } from '../../../../api/get';
+import { RewardRes } from '../../../../types/api/response';
 
 const useReward = (cafeId: number, customerId: number) =>
-  useQuery(
+  useQuery<RewardRes, Error>(
     ['getReward'],
     () => {
       return getReward({ params: { customerId, cafeId } });
     },
     {
       enabled: cafeId !== INVALID_CAFE_ID,
+      refetchOnWindowFocus: false,
+      retry: false,
     },
   );
 
