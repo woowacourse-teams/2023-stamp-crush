7aa6c5fd0756ec06550463ddb2cfb67ff5c27b8a
2yunseong <56749516+2yunseong@users.noreply.github.com>
2023-10-27 13:03:37 +0900

    [FE] refactor: 쿠폰 제작 및 변경 페이지의 로딩/에러 핸들링을 개선한다. (#880)
    * refactor: 이미지 로딩 부분의 로딩을 처리
    
    * refactor: 에러 페이지 구현
    
    * refactor: 쿠폰 템플릿 변경 페이지 로딩/에러 개선
    
    * refactor: 스켈레톤 이미지 동일한 이미지로 변경

--- frontend/src/pages/Admin/ModifyCouponPolicy/components/CurrentCouponImages/index.tsx
+++ frontend/src/pages/Admin/ModifyCouponPolicy/components/CurrentCouponImages/index.tsx
@@ -1,29 +1,76 @@
 import useGetCouponDesign from '../../../ManageCafe/hooks/useGetCouponDesign';
-import { CouponImageFrame, CurrentCouponContainer, Image, StampImageFrame } from './style';
-import LoadingSpinner from '../../../../../components/LoadingSpinner';
+import {
+  CouponImageFrame,
+  CurrentCouponContainer,
+  ErrorContainer,
+  Image,
+  StampImageFrame,
+} from './style';
 import { useRedirectRegisterPage } from '../../../../../hooks/useRedirectRegisterPage';
+import CouponLoadImg from '../../../../../assets/coupon_loading_img.png';
+import StampLoadImg from '../../../../../assets/stamp_load_img.png';
+import { UseQueryResult } from '@tanstack/react-query';
+import { CouponDesign } from '../../../../../types/domain/coupon';
+import Button from '../../../../../components/Button';
+
+type ImgType = 'front' | 'back' | 'stamp';
+
+const getImgURLByStatus = (query: UseQueryResult<CouponDesign, unknown>, imgType: ImgType) => {
+  if (query.status === 'loading') {
+    return imgType === 'stamp' ? StampLoadImg : CouponLoadImg;
+  }
+
+  if (query.status === 'error') {
+    throw new Error('요청에 실패하였습니다.');
+  }
+
+  switch (imgType) {
+    case 'front':
+      return query.data.frontImageUrl;
+    case 'back':
+      return query.data.backImageUrl;
+    case 'stamp':
+      return query.data.stampImageUrl;
+    default:
+      throw new Error('잘못된 이미지 타입을 매개변수로 전달했습니다.');
+  }
+};
 
 const CurrentCouponImages = () => {
   const cafeId = useRedirectRegisterPage();
-  const { data: couponDesignData, status } = useGetCouponDesign(cafeId);
+  const couponDesign = useGetCouponDesign(cafeId);
 
-  if (status === 'error') return <div>Error</div>;
-  if (status === 'loading') return <LoadingSpinner />;
+  const refetchCouponDesign = () => {
+    couponDesign.refetch();
+  };
+
+  if (couponDesign.status === 'error')
+    return (
+      <CurrentCouponContainer>
+        <ErrorContainer>
+          <div>오류가 발생했습니다. :(</div>
+          <div>아래 새로 고침 버튼을 눌러주세요.</div>
+          <div>쿠폰 미리보기를 확인하지 않으셔도</div>
+          <div>쿠폰 정책을 변경하실 수 있습니다.</div>
+          <Button onClick={refetchCouponDesign}>새로 고침</Button>
+        </ErrorContainer>
+      </CurrentCouponContainer>
+    );
 
   return (
     <CurrentCouponContainer>
       <h1>현재 쿠폰 이미지</h1>
       <span>앞면</span>
       <CouponImageFrame>
-        <Image src={couponDesignData?.frontImageUrl} />
+        <Image src={getImgURLByStatus(couponDesign, 'front')} />
       </CouponImageFrame>
       <span>뒷면</span>
       <CouponImageFrame>
-        <Image src={couponDesignData?.backImageUrl} />
+        <Image src={getImgURLByStatus(couponDesign, 'back')} />
       </CouponImageFrame>
       <span>스탬프</span>
       <StampImageFrame>
-        <Image src={couponDesignData?.stampImageUrl} />
+        <Image src={getImgURLByStatus(couponDesign, 'stamp')} />
       </StampImageFrame>
     </CurrentCouponContainer>
   );
