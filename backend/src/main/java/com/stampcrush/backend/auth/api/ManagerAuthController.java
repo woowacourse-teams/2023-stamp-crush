b5f468074da9e10cd003698be7b53aaf37393e57
Donghun Shin <huipulci1@naver.com>
2023-11-12 23:51:57 +0900

    [BE] feat: 리프레시 토큰을 쿠키를 통해 전달 (#928) (#932)

--- backend/src/main/java/com/stampcrush/backend/auth/api/ManagerAuthController.java
+++ backend/src/main/java/com/stampcrush/backend/auth/api/ManagerAuthController.java
@@ -1,9 +1,14 @@
 package com.stampcrush.backend.auth.api;
 
+import static org.springframework.boot.web.server.Cookie.SameSite.NONE;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+
 import com.stampcrush.backend.auth.api.request.OAuthRegisterOwnerCreateRequest;
 import com.stampcrush.backend.auth.api.response.AuthTokensResponse;
 import com.stampcrush.backend.auth.application.ManagerAuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,14 +24,21 @@ public class ManagerAuthController {
 
     @PostMapping("/register/test/token")
     public ResponseEntity<AuthTokensResponse> joinManager(
-            @RequestBody OAuthRegisterOwnerCreateRequest request
+            @RequestBody OAuthRegisterOwnerCreateRequest request,
+            HttpServletResponse response
     ) {
-        return ResponseEntity.ok().body(
-                managerAuthService.join(
-                        request.getNickname(),
-                        request.getoAuthProvider(),
-                        request.getoAuthId()
-                )
+        AuthTokensResponse tokenResponse = managerAuthService.join(
+                request.getNickname(),
+                request.getoAuthProvider(),
+                request.getoAuthId()
         );
+        ResponseCookie cookie = ResponseCookie.from("REFRESH_TOKEN", tokenResponse.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/api/admin/auth/reissue-token")
+                .sameSite(NONE.attributeValue())
+                .build();
+        response.addHeader(SET_COOKIE, cookie.toString());
+        return ResponseEntity.ok(tokenResponse);
     }
 }
