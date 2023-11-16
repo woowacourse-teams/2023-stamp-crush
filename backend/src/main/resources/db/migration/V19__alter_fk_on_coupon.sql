d649fe991c4e9fee72e62c9876544f9ce8d87ee5
깃짱 <107979804+gitchannn@users.noreply.github.com>
2023-11-16 14:01:17 +0900

    [BE] fix: 스크립트 롤백 (FK 설정 관련 에러) (#944)

--- backend/src/main/resources/db/migration/V19__alter_fk_on_coupon.sql
+++
@@ -1,9 +0,0 @@
-alter table coupon
-    add constraint cafe_coupon_design_fk
-        foreign key (cafe_coupon_design_id)
-            references cafe_coupon_design;
-
-alter table coupon
-    add constraint cafe_policy_fk
-        foreign key (cafe_policy_id)
-            references cafe_policy;
