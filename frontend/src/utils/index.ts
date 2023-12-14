01653c9539b908f0b8ff852f4c3a733e5510c5d8
박정규 <srk99279@naver.com>
2023-12-14 10:04:59 +0900

    [FE] hotfix: 날짜 표기 오류를 수정한다 (#959)

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -7,12 +7,12 @@ export const formatDate = (dateString: string) => {
   const dateArray = dateString.split(':');
 
   const year = parseInt(dateArray[0]);
-  const month = parseInt(dateArray[1]);
+  const month = parseInt(dateArray[1]) - 1;
   const day = parseInt(dateArray[2]);
 
   const date = new Date(year, month, day);
 
-  const formattedDate = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`;
+  const formattedDate = `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 
   return formattedDate;
 };
